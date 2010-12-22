08b9e4a82e84219799ea4be6fcb3edccf0a1ad46
Dave Syer <dsyer@vmware.com>
2010-12-22 12:06:46 +0000

    Add tx section

--- spring-amqp-core/src/main/java/org/springframework/amqp/core/Address.java
+++ spring-amqp-core/src/main/java/org/springframework/amqp/core/Address.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2002-2010 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package org.springframework.amqp.core;
@@ -22,11 +19,10 @@ import java.util.regex.Pattern;
 import org.springframework.util.StringUtils;
 
 /**
- * Represents an address for publication of an AMQP message. The AMQP 0-8 and
- * 0-9 specifications have an unstructured string that is used as a "reply to"
- * address. There are however conventions in use and this class makes it easier
- * to follow these conventions.
- *
+ * Represents an address for publication of an AMQP message. The AMQP 0-8 and 0-9 specifications have an unstructured
+ * string that is used as a "reply to" address. There are however conventions in use and this class makes it easier to
+ * follow these conventions.
+ * 
  * @author Mark Pollack
  * @author Mark Fisher
  */
@@ -34,53 +30,55 @@ public class Address {
 
 	private static final Pattern pattern = Pattern.compile("^([^:]+)://([^/]*)/?(.*)$");
 
-
 	private final String exchangeType;
 
 	private final String exchangeName;
 
 	private final String routingKey;
 
-
 	/**
-	 * Create an Address instance from an unstructured String.
-	 * @param address an unstructured string.
+	 * Create an Address instance from a structured String in the form
+	 * 
+	 * <pre>
+	 * (exchangeType)://(exchange)/(routingKey)
+	 * </pre>
+	 * 
+	 * where examples of valid <code>exchangeType</code> values can be found in the {@link ExchangeTypes} static
+	 * constants.
+	 * 
+	 * @param address a structured string.
 	 */
 	public Address(String address) {
 		if (address == null) {
 			this.exchangeType = ExchangeTypes.DIRECT;
 			this.exchangeName = "";
 			this.routingKey = "";
-		}
-		else {
+		} else {
 			Matcher matcher = pattern.matcher(address);
-			boolean matchFound = matcher.find();			
+			boolean matchFound = matcher.find();
 			if (matchFound) {
 				this.exchangeType = matcher.group(1);
 				this.exchangeName = matcher.group(2);
 				this.routingKey = matcher.group(3);
-			}
-			else {
+			} else {
 				this.exchangeType = ExchangeTypes.DIRECT;
 				this.exchangeName = "";
-				this.routingKey = address;				
+				this.routingKey = address;
 			}
-		} 
+		}
 	}
-	
+
 	/***
-	 * Create an Address given the exchange type, exchange name and routing key.  This
-	 * will set the 
+	 * Create an Address given the exchange type, exchange name and routing key. This will set the
 	 * @param exchangeType
 	 * @param exchangeName
 	 * @param routingKey
 	 */
-	public Address(String exchangeType, String exchangeName, String routingKey) {		
+	public Address(String exchangeType, String exchangeName, String routingKey) {
 		this.exchangeType = exchangeType;
 		this.exchangeName = exchangeName;
 		this.routingKey = routingKey;
 	}
-
 
 	public String getExchangeType() {
 		return this.exchangeType;
@@ -93,7 +91,6 @@ public class Address {
 	public String getRoutingKey() {
 		return this.routingKey;
 	}
-
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder(this.exchangeType + "://" + this.exchangeName + "/");
