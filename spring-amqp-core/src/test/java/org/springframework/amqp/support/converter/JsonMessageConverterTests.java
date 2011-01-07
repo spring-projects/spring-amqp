5a137fdb1d8e48fed179eb322b4b5d5e31174035
Dave Syer <dsyer@vmware.com>
2011-01-07 13:12:00 +0000

    Add extra tests for Json converter

--- spring-amqp-core/src/test/java/org/springframework/amqp/support/converter/JsonMessageConverterTests.java
+++ spring-amqp-core/src/test/java/org/springframework/amqp/support/converter/JsonMessageConverterTests.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2002-2010 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package org.springframework.amqp.support.converter;
@@ -22,7 +19,6 @@ import java.math.BigDecimal;
 import java.util.Hashtable;
 
 import org.junit.Test;
-
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 
@@ -50,7 +46,20 @@ public class JsonMessageConverterTests {
 		SimpleTrade marshalledTrade = (SimpleTrade) converter.fromMessage(message);
 		assertEquals(trade, marshalledTrade);
 	}
-	
+
+	@Test
+	public void nestedBean() {
+		Bar bar = new Bar();
+		bar.getFoo().setName("spam");
+		JsonMessageConverter converter = new JsonMessageConverter();
+		Message message = converter.toMessage(bar, new MessageProperties());
+		String classIdFieldName = converter.getClassMapper().getClassIdFieldName();
+		Object classIdHeaderObject = message.getMessageProperties().getHeaders().get(classIdFieldName);
+		assertEquals(String.class, classIdHeaderObject.getClass());
+		Bar marshalled = (Bar) converter.fromMessage(message);
+		assertEquals(bar, marshalled);
+	}
+
 	@Test
 	@SuppressWarnings("unchecked")
 	public void hashtable() {
@@ -62,6 +71,95 @@ public class JsonMessageConverterTests {
 		Hashtable<String, String> marhsalledHashtable = (Hashtable<String, String>) converter.fromMessage(message);
 		assertEquals("VMW", marhsalledHashtable.get("TICKER"));
 		assertEquals("103.2", marhsalledHashtable.get("PRICE"));
+	}
+
+	public static class Foo {
+		private String name = "foo";
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Foo other = (Foo) obj;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			return true;
+		}
+	}
+
+	public static class Bar {
+		private String name = "bar";
+		private Foo foo = new Foo();
+
+		public Foo getFoo() {
+			return foo;
+		}
+
+		public void setFoo(Foo foo) {
+			this.foo = foo;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((foo == null) ? 0 : foo.hashCode());
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Bar other = (Bar) obj;
+			if (foo == null) {
+				if (other.foo != null)
+					return false;
+			} else if (!foo.equals(other.foo))
+				return false;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			return true;
+		}
 	}
 
 }
