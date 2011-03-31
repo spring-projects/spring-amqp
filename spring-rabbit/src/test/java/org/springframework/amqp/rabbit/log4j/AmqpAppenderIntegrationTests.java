1956dca3e8b3b1da55f892ab6ec8d56bf8dfb44c
Dave Syer <dsyer@vmware.com>
2011-03-31 09:34:13 +0100

    AMQP-137: Align logger test and appender internals with project norms

---
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/log4j/AmqpAppenderIntegrationTests.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2011 by the original author(s).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.springframework.amqp.rabbit.log4j;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.MDC;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.test.BrokerRunning;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Log4jConfigurer;
+
+/**
+ * @author Jon Brisbin <jbrisbin@vmware.com>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "org.springframework.amqp.rabbit.log4j" }, loader = AnnotationConfigContextLoader.class)
+public class AmqpAppenderIntegrationTests {
+
+	@Rule
+	public BrokerRunning brokerIsRunning = BrokerRunning.isRunning();
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	private Logger log;
+
+	private SimpleMessageListenerContainer listenerContainer;
+
+	@Before
+	public void setUp() throws Exception {
+		Log4jConfigurer.initLogging("classpath:log4j-amqp.properties");
+		log = Logger.getLogger(getClass());
+		listenerContainer = applicationContext.getBean(SimpleMessageListenerContainer.class);
+	}
+
+	@After
+	public void tearDown() {
+		listenerContainer.shutdown();
+	}
+
+	@Test
+	public void testAppender() throws InterruptedException {
+		TestListener testListener = (TestListener) applicationContext.getBean("testListener", 4);
+		listenerContainer.setMessageListener(testListener);
+		listenerContainer.start();
+
+		Logger log = Logger.getLogger(getClass());
+
+		log.debug("This is a DEBUG message");
+		log.info("This is an INFO message");
+		log.warn("This is a WARN message");
+		log.error("This is an ERROR message", new RuntimeException("Test exception"));
+
+		testListener.getLatch().await(5, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testAppenderWithProps() throws InterruptedException {
+		TestListener testListener = (TestListener) applicationContext.getBean("testListener", 4);
+		listenerContainer.setMessageListener(testListener);
+		listenerContainer.start();
+
+		MDC.put("someproperty", "property.value");
+		log.debug("This is a DEBUG message with properties");
+		log.info("This is an INFO message with properties");
+		log.warn("This is a WARN message with properties");
+		log.error("This is an ERROR message with properties", new RuntimeException("Test exception"));
+		MDC.remove("someproperty");
+
+		testListener.getLatch().await(5, TimeUnit.SECONDS);
+	}
+
+}
