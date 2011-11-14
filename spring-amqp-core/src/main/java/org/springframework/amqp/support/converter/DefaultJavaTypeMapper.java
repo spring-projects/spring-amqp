6b60ac6b45ec6b40dca4f7fcef84377e349a5a93
samnelson <samnelson@carfax.com>
2011-11-14 08:56:02 -0600

    formatted code after importing eclipse-code-formatter.xml

--- spring-amqp-core/src/main/java/org/springframework/amqp/support/converter/DefaultJavaTypeMapper.java
+++ spring-amqp-core/src/main/java/org/springframework/amqp/support/converter/DefaultJavaTypeMapper.java
@@ -1,22 +1,16 @@
 /*
- * Copyright 2002-2010 the original author or authors.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Copyright 2002-2010 the original author or authors. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 package org.springframework.amqp.support.converter;
 
 /**
- * 
  * @author Mark Pollack
  * @author Sam Nelson
- * 
  */
 import java.util.Collection;
 import java.util.HashMap;
@@ -33,126 +27,112 @@ import org.springframework.util.ClassUtils;
 
 public class DefaultJavaTypeMapper implements JavaTypeMapper, InitializingBean {
 
-	public static final String DEFAULT_CLASSID_FIELD_NAME = "__TypeId__";
-	public static final String DEFAULT_CONTENT_CLASSID_FIELD_NAME = "__ContentTypeId__";
-	public static final String DEFAULT_KEY_CLASSID_FIELD_NAME = "__KeyTypeId__";
-	
-	private Map<String, Class<?>> idClassMapping = new HashMap<String, Class<?>>();
-	private Map<Class<?>, String> classIdMapping = new HashMap<Class<?>, String>();
+   public static final String DEFAULT_CLASSID_FIELD_NAME = "__TypeId__";
+   public static final String DEFAULT_CONTENT_CLASSID_FIELD_NAME = "__ContentTypeId__";
+   public static final String DEFAULT_KEY_CLASSID_FIELD_NAME = "__KeyTypeId__";
 
-	public String getClassIdFieldName() {
-		return DEFAULT_CLASSID_FIELD_NAME;
-	}
-	
-	public String getContentClassIdFieldName() {
-		return DEFAULT_CONTENT_CLASSID_FIELD_NAME;
-	}
+   private Map<String, Class<?>> idClassMapping = new HashMap<String, Class<?>>();
+   private Map<Class<?>, String> classIdMapping = new HashMap<Class<?>, String>();
 
-	public String getKeyClassIdFieldName() {
-		return DEFAULT_KEY_CLASSID_FIELD_NAME;
-	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public JavaType toJavaType(MessageProperties properties) {
-		JavaType classType = getClassIdType(retrieveHeader(properties,
-				getClassIdFieldName()));
-		if (!classType.isContainerType()) {
-			return classType;
-		}
+   public String getClassIdFieldName() {
+      return DEFAULT_CLASSID_FIELD_NAME;
+   }
 
-		JavaType contentClassType = getClassIdType(retrieveHeader(properties,
-				getContentClassIdFieldName()));
-		if (classType.getKeyType() == null) {
-			return TypeFactory.collectionType(
-					(Class<? extends Collection>) classType.getRawClass(),
-					contentClassType);
-		}
-		
-		JavaType keyClassType = getClassIdType(retrieveHeader(properties, getKeyClassIdFieldName()));
-		JavaType mapType = TypeFactory.mapType(
-				(Class<? extends Map>) classType.getRawClass(), keyClassType, 
-				contentClassType);
-		return mapType;
-		
-	}
+   public String getContentClassIdFieldName() {
+      return DEFAULT_CONTENT_CLASSID_FIELD_NAME;
+   }
 
-	private JavaType getClassIdType(String classId) {
-		if (this.idClassMapping.containsKey(classId)) {
-			return TypeFactory.type(idClassMapping.get(classId));
-		}
+   public String getKeyClassIdFieldName() {
+      return DEFAULT_KEY_CLASSID_FIELD_NAME;
+   }
 
-		try {
-			return TypeFactory.type(ClassUtils.forName(classId, getClass()
-					.getClassLoader()));
-		} catch (ClassNotFoundException e) {
-			throw new MessageConversionException(
-					"failed to resolve class name. Class not found [" + classId
-							+ "]", e);
-		} catch (LinkageError e) {
-			throw new MessageConversionException(
-					"failed to resolve class name. Linkage error [" + classId
-							+ "]", e);
-		}
-	}
+   @SuppressWarnings({ "unchecked", "rawtypes" })
+   public JavaType toJavaType(MessageProperties properties) {
+      JavaType classType = getClassIdType(retrieveHeader(properties, getClassIdFieldName()));
+      if (!classType.isContainerType()) {
+         return classType;
+      }
 
-	private String retrieveHeader(MessageProperties properties,
-			String headerName) {
-		Map<String, Object> headers = properties.getHeaders();
-		Object classIdFieldNameValue = headers.get(headerName);
-		String classId = null;
-		if (classIdFieldNameValue != null) {
-			classId = classIdFieldNameValue.toString();
-		}
-		if (classId == null) {
-			throw new MessageConversionException(
-					"failed to convert Message content. Could not resolve "
-							+ headerName + " in header");
-		}
-		return classId;
-	}
+      JavaType contentClassType = getClassIdType(retrieveHeader(properties, getContentClassIdFieldName()));
+      if (classType.getKeyType() == null) {
+         return TypeFactory.collectionType((Class<? extends Collection>) classType.getRawClass(), contentClassType);
+      }
 
-	public void setIdClassMapping(Map<String, Class<?>> idClassMapping) {
-		this.idClassMapping = idClassMapping;
-	}
+      JavaType keyClassType = getClassIdType(retrieveHeader(properties, getKeyClassIdFieldName()));
+      JavaType mapType = TypeFactory.mapType((Class<? extends Map>) classType.getRawClass(), keyClassType,
+                                             contentClassType);
+      return mapType;
 
-	public void fromJavaType(JavaType javaType, MessageProperties properties) {
-		addHeader(properties, getClassIdFieldName(),
-				(Class<?>) javaType.getRawClass());
+   }
 
-		if (javaType.isContainerType()) {
-			addHeader(properties, getContentClassIdFieldName(), javaType
-					.getContentType().getRawClass());
-		}
-		
-		if (javaType.getKeyType() != null) {
-			addHeader(properties, getKeyClassIdFieldName(), javaType
-					.getKeyType().getRawClass());
-		}
-	}
-	
-	public void afterPropertiesSet() throws Exception {
-		validateIdTypeMapping();
-	}
+   private JavaType getClassIdType(String classId) {
+      if (this.idClassMapping.containsKey(classId)) {
+         return TypeFactory.type(idClassMapping.get(classId));
+      }
 
+      try {
+         return TypeFactory.type(ClassUtils.forName(classId, getClass().getClassLoader()));
+      }
+      catch (ClassNotFoundException e) {
+         throw new MessageConversionException("failed to resolve class name. Class not found [" + classId + "]", e);
+      }
+      catch (LinkageError e) {
+         throw new MessageConversionException("failed to resolve class name. Linkage error [" + classId + "]", e);
+      }
+   }
 
-	private void addHeader(MessageProperties properties, String headerName,
-			Class<?> clazz) {
-		if (classIdMapping.containsKey(clazz)) {
-			properties.getHeaders().put(headerName, classIdMapping.get(clazz));
-		} else {
-			properties.getHeaders().put(headerName, clazz.getName());
-		}
-	}
+   private String retrieveHeader(MessageProperties properties, String headerName) {
+      Map<String, Object> headers = properties.getHeaders();
+      Object classIdFieldNameValue = headers.get(headerName);
+      String classId = null;
+      if (classIdFieldNameValue != null) {
+         classId = classIdFieldNameValue.toString();
+      }
+      if (classId == null) {
+         throw new MessageConversionException("failed to convert Message content. Could not resolve "
+                                              + headerName
+                                              + " in header");
+      }
+      return classId;
+   }
 
-	
-	private void validateIdTypeMapping() {
-		Map<String, Class<?>> finalIdClassMapping = new HashMap<String, Class<?>>();
-		for (Entry<String, Class<?>> entry : idClassMapping.entrySet()) {
-			String id = entry.getKey();
-			Class<?> clazz = entry.getValue();
-			finalIdClassMapping.put(id, clazz);
-			classIdMapping.put(clazz, id);
-		}
-		this.idClassMapping = finalIdClassMapping;
-	}
+   public void setIdClassMapping(Map<String, Class<?>> idClassMapping) {
+      this.idClassMapping = idClassMapping;
+   }
+
+   public void fromJavaType(JavaType javaType, MessageProperties properties) {
+      addHeader(properties, getClassIdFieldName(), (Class<?>) javaType.getRawClass());
+
+      if (javaType.isContainerType()) {
+         addHeader(properties, getContentClassIdFieldName(), javaType.getContentType().getRawClass());
+      }
+
+      if (javaType.getKeyType() != null) {
+         addHeader(properties, getKeyClassIdFieldName(), javaType.getKeyType().getRawClass());
+      }
+   }
+
+   public void afterPropertiesSet() throws Exception {
+      validateIdTypeMapping();
+   }
+
+   private void addHeader(MessageProperties properties, String headerName, Class<?> clazz) {
+      if (classIdMapping.containsKey(clazz)) {
+         properties.getHeaders().put(headerName, classIdMapping.get(clazz));
+      }
+      else {
+         properties.getHeaders().put(headerName, clazz.getName());
+      }
+   }
+
+   private void validateIdTypeMapping() {
+      Map<String, Class<?>> finalIdClassMapping = new HashMap<String, Class<?>>();
+      for (Entry<String, Class<?>> entry : idClassMapping.entrySet()) {
+         String id = entry.getKey();
+         Class<?> clazz = entry.getValue();
+         finalIdClassMapping.put(id, clazz);
+         classIdMapping.put(clazz, id);
+      }
+      this.idClassMapping = finalIdClassMapping;
+   }
 }
--- spring-amqp-core/src/main/java/org/springframework/amqp/support/converter/JsonMessageConverter.java
+++ spring-amqp-core/src/main/java/org/springframework/amqp/support/converter/JsonMessageConverter.java
@@ -1,14 +1,10 @@
 /*
- * Copyright 2002-2010 the original author or authors.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Copyright 2002-2010 the original author or authors. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 
 package org.springframework.amqp.support.converter;
@@ -38,115 +34,125 @@ import org.springframework.amqp.core.MessageProperties;
  */
 public class JsonMessageConverter extends AbstractMessageConverter {
 
-	private static Log log = LogFactory.getLog(JsonMessageConverter.class);
+   private static Log log = LogFactory.getLog(JsonMessageConverter.class);
 
-	public static final String DEFAULT_CHARSET = "UTF-8";
+   public static final String DEFAULT_CHARSET = "UTF-8";
 
-	private volatile String defaultCharset = DEFAULT_CHARSET;
+   private volatile String defaultCharset = DEFAULT_CHARSET;
 
-	private ObjectMapper jsonObjectMapper = new ObjectMapper();
+   private ObjectMapper jsonObjectMapper = new ObjectMapper();
 
-	private JavaTypeMapper javaTypeMapper = new DefaultJavaTypeMapper();
+   private JavaTypeMapper javaTypeMapper = new DefaultJavaTypeMapper();
 
+   public JsonMessageConverter() {
+      super();
+      initializeJsonObjectMapper();
+   }
 
-	public JsonMessageConverter() {
-		super();
-		initializeJsonObjectMapper();
-	}
+   /**
+    * Specify the default charset to use when converting to or from text-based Message body content. If not specified,
+    * the charset will be "UTF-8".
+    */
+   public void setDefaultCharset(String defaultCharset) {
+      this.defaultCharset = (defaultCharset != null) ? defaultCharset : DEFAULT_CHARSET;
+   }
 
-	/**
-	 * Specify the default charset to use when converting to or from text-based Message body content. If not specified,
-	 * the charset will be "UTF-8".
-	 */
-	public void setDefaultCharset(String defaultCharset) {
-		this.defaultCharset = (defaultCharset != null) ? defaultCharset : DEFAULT_CHARSET;
-	}
+   public JavaTypeMapper getJavaTypeMapper() {
+      return javaTypeMapper;
+   }
 
-	public JavaTypeMapper getJavaTypeMapper() {
-		return javaTypeMapper;
-	}
-	
-	public void setJavaTypeMapper(JavaTypeMapper javaTypeMapper) {
-		this.javaTypeMapper = javaTypeMapper;
-	}
+   public void setJavaTypeMapper(JavaTypeMapper javaTypeMapper) {
+      this.javaTypeMapper = javaTypeMapper;
+   }
 
-	/**
-	 * The {@link ObjectMapper} to use instead of using the default. An alternative to injecting a mapper is to extend
-	 * this class and override {@link #initializeJsonObjectMapper()}.
-	 * 
-	 * @param jsonObjectMapper the object mapper to set
-	 */
-	public void setJsonObjectMapper(ObjectMapper jsonObjectMapper) {
-		this.jsonObjectMapper = jsonObjectMapper;
-	}
+   /**
+    * The {@link ObjectMapper} to use instead of using the default. An alternative to injecting a mapper is to extend
+    * this class and override {@link #initializeJsonObjectMapper()}.
+    * 
+    * @param jsonObjectMapper the object mapper to set
+    */
+   public void setJsonObjectMapper(ObjectMapper jsonObjectMapper) {
+      this.jsonObjectMapper = jsonObjectMapper;
+   }
 
-	/**
-	 * Subclass and override to customize.
-	 */
-	protected void initializeJsonObjectMapper() {
-		jsonObjectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	}
+   /**
+    * Subclass and override to customize.
+    */
+   protected void initializeJsonObjectMapper() {
+      jsonObjectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+   }
 
-	public Object fromMessage(Message message) throws MessageConversionException {
-		Object content = null;
-		MessageProperties properties = message.getMessageProperties();
-		if (properties != null) {
-			String contentType = properties.getContentType();
-			if (contentType != null && contentType.contains("json")) {
-				String encoding = properties.getContentEncoding();
-				if (encoding == null) {
-					encoding = this.defaultCharset;
-				}
-				try {
-					JavaType targetClass = javaTypeMapper.toJavaType(message.getMessageProperties());
-					content = convertBytesToObject(message.getBody(), encoding, targetClass);
-				} catch (UnsupportedEncodingException e) {
-					throw new MessageConversionException("Failed to convert json-based Message content", e);
-				} catch (JsonParseException e) {
-					throw new MessageConversionException("Failed to convert Message content", e);
-				} catch (JsonMappingException e) {
-					throw new MessageConversionException("Failed to convert Message content", e);
-				} catch (IOException e) {
-					throw new MessageConversionException("Failed to convert Message content", e);
-				}
-			} else {
-				log.warn("Could not convert incoming message with content-type [" + contentType + "]");
-			}
-		}
-		if (content == null) {
-			content = message.getBody();
-		}
-		return content;
-	}
+   public Object fromMessage(Message message) throws MessageConversionException {
+      Object content = null;
+      MessageProperties properties = message.getMessageProperties();
+      if (properties != null) {
+         String contentType = properties.getContentType();
+         if (contentType != null && contentType.contains("json")) {
+            String encoding = properties.getContentEncoding();
+            if (encoding == null) {
+               encoding = this.defaultCharset;
+            }
+            try {
+               JavaType targetClass = javaTypeMapper.toJavaType(message.getMessageProperties());
+               content = convertBytesToObject(message.getBody(), encoding, targetClass);
+            }
+            catch (UnsupportedEncodingException e) {
+               throw new MessageConversionException("Failed to convert json-based Message content", e);
+            }
+            catch (JsonParseException e) {
+               throw new MessageConversionException("Failed to convert Message content", e);
+            }
+            catch (JsonMappingException e) {
+               throw new MessageConversionException("Failed to convert Message content", e);
+            }
+            catch (IOException e) {
+               throw new MessageConversionException("Failed to convert Message content", e);
+            }
+         }
+         else {
+            log.warn("Could not convert incoming message with content-type [" + contentType + "]");
+         }
+      }
+      if (content == null) {
+         content = message.getBody();
+      }
+      return content;
+   }
 
-	private Object convertBytesToObject(byte[] body, String encoding, JavaType targetJavaType) throws JsonParseException,
-			JsonMappingException, IOException {
-		String contentAsString = new String(body, encoding);
-		return jsonObjectMapper.readValue(contentAsString, targetJavaType);
-	}
+   private Object convertBytesToObject(byte[] body, String encoding, JavaType targetJavaType)
+                                                                                             throws JsonParseException,
+                                                                                             JsonMappingException,
+                                                                                             IOException {
+      String contentAsString = new String(body, encoding);
+      return jsonObjectMapper.readValue(contentAsString, targetJavaType);
+   }
 
-	protected Message createMessage(Object objectToConvert, MessageProperties messageProperties)
-			throws MessageConversionException {
-		byte[] bytes = null;
-		try {
-			String jsonString = jsonObjectMapper.writeValueAsString(objectToConvert);
-			bytes = jsonString.getBytes(this.defaultCharset);
-		} catch (UnsupportedEncodingException e) {
-			throw new MessageConversionException("Failed to convert Message content", e);
-		} catch (JsonGenerationException e) {
-			throw new MessageConversionException("Failed to convert Message content", e);
-		} catch (JsonMappingException e) {
-			throw new MessageConversionException("Failed to convert Message content", e);
-		} catch (IOException e) {
-			throw new MessageConversionException("Failed to convert Message content", e);
-		}
-		messageProperties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-		messageProperties.setContentEncoding(this.defaultCharset);
-		if (bytes != null) {
-			messageProperties.setContentLength(bytes.length);
-		}
-		javaTypeMapper.fromJavaType(TypeFactory.type(objectToConvert.getClass()), messageProperties);
-		return new Message(bytes, messageProperties);
-	}
+   protected Message createMessage(Object objectToConvert, MessageProperties messageProperties)
+                                                                                               throws MessageConversionException {
+      byte[] bytes = null;
+      try {
+         String jsonString = jsonObjectMapper.writeValueAsString(objectToConvert);
+         bytes = jsonString.getBytes(this.defaultCharset);
+      }
+      catch (UnsupportedEncodingException e) {
+         throw new MessageConversionException("Failed to convert Message content", e);
+      }
+      catch (JsonGenerationException e) {
+         throw new MessageConversionException("Failed to convert Message content", e);
+      }
+      catch (JsonMappingException e) {
+         throw new MessageConversionException("Failed to convert Message content", e);
+      }
+      catch (IOException e) {
+         throw new MessageConversionException("Failed to convert Message content", e);
+      }
+      messageProperties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
+      messageProperties.setContentEncoding(this.defaultCharset);
+      if (bytes != null) {
+         messageProperties.setContentLength(bytes.length);
+      }
+      javaTypeMapper.fromJavaType(TypeFactory.type(objectToConvert.getClass()), messageProperties);
+      return new Message(bytes, messageProperties);
+   }
 
 }
