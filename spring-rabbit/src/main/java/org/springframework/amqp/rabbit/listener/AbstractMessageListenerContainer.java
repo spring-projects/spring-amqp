b75318e64ce4d8bad35a07e807ea6656b507eb0b
James Carr <james.r.carr@gmail.com>
2011-11-15 11:09:28 -0600

    changed isDebugEnabled and isInfoEnabled to isWarnEnabled.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/AbstractMessageListenerContainer.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/AbstractMessageListenerContainer.java
@@ -423,9 +423,9 @@ public abstract class AbstractMessageListenerContainer extends RabbitAccessor im
 	protected void invokeErrorHandler(Throwable ex) {
 		if (this.errorHandler != null) {
 			this.errorHandler.handleError(ex);
-		} else if (logger.isDebugEnabled()) {
+		} else if (logger.isWarnEnabled()) {
 			logger.warn("Execution of Rabbit message listener failed, and no ErrorHandler has been set.", ex);
-		} else if (logger.isInfoEnabled()) {
+		} else if (logger.isWarnEnabled()) {
 			logger.warn("Execution of Rabbit message listener failed, and no ErrorHandler has been set: "
 					+ ex.getClass() + ": " + ex.getMessage());
 		}
