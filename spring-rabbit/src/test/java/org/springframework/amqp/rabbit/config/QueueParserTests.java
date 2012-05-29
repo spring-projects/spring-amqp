38049201e0500bd7b236889059f25408defcf850
Gary Russell <grussell@vmware.com>
2012-05-29 16:14:21 -0400

    AMQP-198 Document Mixed Queue Arg Types
    Need to declare the type when setting non-string arguments,
    such as x-message-ttl.
    
    Add test showing how to configure a map of mixed types.

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/config/QueueParserTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/config/QueueParserTests.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2008 the original author or authors.
- * 
+ * Copyright 2002-2012 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -91,6 +91,8 @@ public class QueueParserTests {
 		Queue queue = beanFactory.getBean("arguments", Queue.class);
 		assertNotNull(queue);
 		assertEquals("bar", queue.getArguments().get("foo"));
+		assertEquals(100L, queue.getArguments().get("x-message-ttl"));
+		assertEquals("all", queue.getArguments().get("x-ha-policy"));
 	}
 
 	@Test
