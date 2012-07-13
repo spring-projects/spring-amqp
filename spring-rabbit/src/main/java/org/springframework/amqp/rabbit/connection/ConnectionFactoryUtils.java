4e165eacd2dba8dcc2e618d0307dde449b4a443b
Gary Russell <grussell@vmware.com>
2012-07-13 18:53:06 -0400

    AMQP-190 Memory Leak With Tx and RabbitTemplate
    When the RabbitTemplate is invoked with an existing transaction,
    the channel is bound to the thread, and
    
      ConnectionFactoryUtils.releaseResources(resourceHolder);
    
    is called after processing. There was a commented-out line
    in ConnectionFactoryUtils.RabbitResourceSynchronization.afterCompletion()
    that would have reset the synchronized state so that
    releaseResouces() would "close" the chanel/connection (return
    them to the caching factory).
    
    Being commented out, the channel was never removed, and each
    transactional template call grabbed a new channel.
    
    However, uncommenting causes issues with the listener container
    because it continues to use the channel, so it must not
    be closed (made available for reuse).
    
    Added code to reset synch, by default, but not for the listener.
    
    The listener sets a boolean releaseAfterCompletion in the
    ResourceHolder to false so that the channel remains and
    is not closed (logically or otherwise).

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/ConnectionFactoryUtils.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/ConnectionFactoryUtils.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2010 the original author or authors.
- * 
+ * Copyright 2002-2012 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -26,13 +26,14 @@ import com.rabbitmq.client.Channel;
 /**
  * Helper class for managing a Spring based Rabbit {@link org.springframework.amqp.rabbit.connection.ConnectionFactory},
  * in particular for obtaining transactional Rabbit resources for a given ConnectionFactory.
- * 
+ *
  * <p>
  * Mainly for internal use within the framework. Used by {@link org.springframework.amqp.rabbit.core.RabbitTemplate} as
  * well as {@link org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer}.
- * 
+ *
  * @author Mark Fisher
  * @author Dave Syer
+ * @author Gary Russell
  */
 public class ConnectionFactoryUtils {
 
@@ -161,7 +162,7 @@ public class ConnectionFactoryUtils {
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	public static void registerDeliveryTag(ConnectionFactory connectionFactory, Channel channel, Long tag)
 			throws IOException {
@@ -237,10 +238,12 @@ public class ConnectionFactoryUtils {
 			this.transacted = transacted;
 		}
 
+		@Override
 		protected boolean shouldReleaseBeforeCompletion() {
 			return !this.transacted;
 		}
 
+		@Override
 		protected void processResourceAfterCommit(RabbitResourceHolder resourceHolder) {
 			resourceHolder.commitAll();
 		}
@@ -250,10 +253,13 @@ public class ConnectionFactoryUtils {
 			if (status != TransactionSynchronization.STATUS_COMMITTED) {
 				resourceHolder.rollbackAll();
 			}
-			// resourceHolder.setSynchronizedWithTransaction(false);
+			if (resourceHolder.isReleaseAfterCompletion()) {
+				resourceHolder.setSynchronizedWithTransaction(false);
+			}
 			super.afterCompletion(status);
 		}
 
+		@Override
 		protected void releaseResource(RabbitResourceHolder resourceHolder, Object resourceKey) {
 			ConnectionFactoryUtils.releaseResources(resourceHolder);
 		}
