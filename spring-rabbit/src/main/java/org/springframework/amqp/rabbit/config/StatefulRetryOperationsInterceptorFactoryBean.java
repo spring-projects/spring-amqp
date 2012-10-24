57b34b982ad8be3744f4f8842d873edf68e97f62
Gary Russell <grussell@vmware.com>
2012-10-24 18:21:27 -0400

    AMQP-272 Fix Typo In Method Name
    Deprecated old method.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/StatefulRetryOperationsInterceptorFactoryBean.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/StatefulRetryOperationsInterceptorFactoryBean.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2010 the original author or authors.
- * 
+ * Copyright 2002-2012 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -37,12 +37,13 @@ import org.springframework.retry.support.RetryTemplate;
  * be recognised (hence the {@link MessageKeyGenerator} strategy), and when the retry attempts are exhausted it will be
  * processed using a {@link MessageRecoverer} if one is provided, in a new transaction. If a recoverer is not provided
  * the message will be logged and dropped.
- * 
+ *
  * @see RetryOperations#execute(org.springframework.retry.RetryCallback, org.springframework.retry.RecoveryCallback,
  * org.springframework.retry.RetryState)
- * 
+ *
  * @author Dave Syer
- * 
+ * @author Gary Russell
+ *
  */
 public class StatefulRetryOperationsInterceptorFactoryBean extends AbstractRetryOperationsInterceptorFactoryBean {
 
@@ -52,7 +53,15 @@ public class StatefulRetryOperationsInterceptorFactoryBean extends AbstractRetry
 
 	private NewMessageIdentifier newMessageIdentifier;
 
+	/**
+	 * @deprecated Use {@link #setMessageKeyGenerator(MessageKeyGenerator)} instead
+	 */
+	@Deprecated
 	public void setMessageKeyGeneretor(MessageKeyGenerator messageKeyGeneretor) {
+		setMessageKeyGenerator(messageKeyGeneretor);
+	}
+
+	public void setMessageKeyGenerator(MessageKeyGenerator messageKeyGeneretor) {
 		this.messageKeyGenerator = messageKeyGeneretor;
 	}
 
@@ -120,6 +129,7 @@ public class StatefulRetryOperationsInterceptorFactoryBean extends AbstractRetry
 		return StatefulRetryOperationsInterceptor.class;
 	}
 
+	@Override
 	public boolean isSingleton() {
 		return true;
 	}
