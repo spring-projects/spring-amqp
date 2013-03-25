0ac827e73a7588deee79e28c6c517eaadcb3086e
Gunnar Hillert <ghillert@vmware.com>
2013-03-25 18:05:51 -0400

    AMQP-302 - Remove Warnings During Build with JDK7
    * Add JavaDoc links to build.gradle
    * Make deprecation message in *FederatedExchange* more expressive (Add affected version)
    
    AMQP-302 - Code Review Changes

--- spring-amqp/src/main/java/org/springframework/amqp/core/FederatedExchange.java
+++ spring-amqp/src/main/java/org/springframework/amqp/core/FederatedExchange.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,8 @@ import java.util.Map;
 /**
  *
  * @see AmqpAdmin
- * @deprecated RabbitMQ no longer supports 'x-federation' exchanges.
+ * @deprecated Starting with version 3.0 RabbitMQ no longer supports
+ * 'x-federation' exchanges.
  *
  * @author Gary Russell
  */
--- spring-amqp/src/main/java/org/springframework/amqp/support/converter/SimpleMessageConverter.java
+++ spring-amqp/src/main/java/org/springframework/amqp/support/converter/SimpleMessageConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2010 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +28,7 @@ import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.utils.SerializationUtils;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.remoting.rmi.CodebaseAwareObjectInputStream;
+import java.rmi.server.RMIClassLoader;
 import org.springframework.util.ClassUtils;
 
 /**
