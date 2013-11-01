c3965580bc24020cb2cdc8ebd99011fc0eabb705
Gary Russell <grussell@gopivotal.com>
2013-11-01 13:08:01 -0400

    AMQP-339 Support Build with SPR 4.0.0
    - Add refresh() to test GenericContexts
    - Remove commons.lang NotImplementedException
    
    Also increase timer in a test case.
    
    JIRA: https://jira.springsource.org/browse/AMQP-339

--- spring-amqp/src/test/java/org/springframework/amqp/remoting/testhelper/AbstractAmqpTemplate.java
+++ spring-amqp/src/test/java/org/springframework/amqp/remoting/testhelper/AbstractAmqpTemplate.java
@@ -13,7 +13,6 @@
 
 package org.springframework.amqp.remoting.testhelper;
 
-import org.apache.commons.lang.NotImplementedException;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Message;
@@ -27,116 +26,116 @@ public abstract class AbstractAmqpTemplate implements AmqpTemplate {
 
 	@Override
 	public void send(Message message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void send(String routingKey, Message message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void send(String exchange, String routingKey, Message message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void convertAndSend(Object message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void convertAndSend(String routingKey, Object message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void convertAndSend(String exchange, String routingKey, Object message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void convertAndSend(Object message, MessagePostProcessor messagePostProcessor) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void convertAndSend(String routingKey, Object message, MessagePostProcessor messagePostProcessor)
 			throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void convertAndSend(String exchange, String routingKey, Object message,
 			MessagePostProcessor messagePostProcessor) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Message receive() throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Message receive(String queueName) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object receiveAndConvert() throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object receiveAndConvert(String queueName) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Message sendAndReceive(Message message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Message sendAndReceive(String routingKey, Message message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Message sendAndReceive(String exchange, String routingKey, Message message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object convertSendAndReceive(Object message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object convertSendAndReceive(String routingKey, Object message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object convertSendAndReceive(String exchange, String routingKey, Object message) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object convertSendAndReceive(Object message, MessagePostProcessor messagePostProcessor) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object convertSendAndReceive(String routingKey, Object message, MessagePostProcessor messagePostProcessor)
 			throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object convertSendAndReceive(String exchange, String routingKey, Object message,
 			MessagePostProcessor messagePostProcessor) throws AmqpException {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 }
