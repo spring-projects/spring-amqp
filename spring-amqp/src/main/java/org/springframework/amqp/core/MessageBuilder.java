3cdac5f4a90347d466686cbf0e5baa05ae82cca8
Gary Russell <grussell@gopivotal.com>
2014-01-09 12:10:41 -0500

    AMQP-39 Polishing - Use Class Hierarchy
    MessageBuilderSupport (abstract) with subclasses
    MessageBuilder and MessagePropertiesBuilder.

--- spring-amqp/src/main/java/org/springframework/amqp/core/MessageBuilder.java
+++ spring-amqp/src/main/java/org/springframework/amqp/core/MessageBuilder.java
@@ -16,8 +16,6 @@
 package org.springframework.amqp.core;
 
 import java.util.Arrays;
-import java.util.Date;
-import java.util.Map;
 
 import org.springframework.util.Assert;
 
@@ -29,11 +27,9 @@ import org.springframework.util.Assert;
  * @since 1.3
  *
  */
-public final class MessageBuilder {
+public final class MessageBuilder extends MessageBuilderSupport<Message> {
 
 	private final byte[] body;
-
-	private MessagePropertiesBuilder propertiesBuilder = MessagePropertiesBuilder.newInstance();
 
 	/**
 	 * The final message body will be a direct reference to 'body'.
@@ -113,258 +109,13 @@ public final class MessageBuilder {
 	 * @return this.
 	 */
 	public MessageBuilder andProperties(MessageProperties properties) {
-		this.propertiesBuilder = MessagePropertiesBuilder.fromProperties(properties);
+		this.setProperties(properties);
 		return this;
 	}
 
-	/*
-	 * Delegate methods for property builder so users can use fluency with fromMessage...
-	 *
-	 * MessageBuilder.fromMessage(message).setHeaderIfAbsent("foo", "bar").build();
-	 *
-	 */
-
-	public MessageBuilder setHeader(String key, Object value) {
-		this.propertiesBuilder.setHeader(key, value);
-		return this;
-	}
-
-	public MessageBuilder setTimestamp(Date timestamp) {
-		this.propertiesBuilder.setTimestamp(timestamp);
-		return this;
-	}
-
-	public MessageBuilder setMessageId(String messageId) {
-		this.propertiesBuilder.setMessageId(messageId);
-		return this;
-	}
-
-	public MessageBuilder setUserId(String userId) {
-		this.propertiesBuilder.setUserId(userId);
-		return this;
-	}
-
-	public MessageBuilder setAppId(String appId) {
-		this.propertiesBuilder.setAppId(appId);
-		return this;
-	}
-
-	public MessageBuilder setClusterId(String clusterId) {
-		this.propertiesBuilder.setClusterId(clusterId);
-		return this;
-	}
-
-	public MessageBuilder setType(String type) {
-		this.propertiesBuilder.setType(type);
-		return this;
-	}
-
-	public MessageBuilder setCorrelationId(byte[] correlationId) {
-		this.propertiesBuilder.setCorrelationId(correlationId);
-		return this;
-	}
-
-	public MessageBuilder setReplyTo(String replyTo) {
-		this.propertiesBuilder.setReplyTo(replyTo);
-		return this;
-	}
-
-	public MessageBuilder setReplyToAddress(Address replyTo) {
-		this.propertiesBuilder.setReplyToAddress(replyTo);
-		return this;
-	}
-
-	public MessageBuilder setContentType(String contentType) {
-		this.propertiesBuilder.setContentType(contentType);
-		return this;
-	}
-
-	public MessageBuilder setContentEncoding(String contentEncoding) {
-		this.propertiesBuilder.setContentEncoding(contentEncoding);
-		return this;
-	}
-
-	public MessageBuilder setContentLength(long contentLength) {
-		this.propertiesBuilder.setContentLength(contentLength);
-		return this;
-	}
-
-	public MessageBuilder setDeliveryMode(MessageDeliveryMode deliveryMode) {
-		this.propertiesBuilder.setDeliveryMode(deliveryMode);
-		return this;
-	}
-
-	public MessageBuilder setExpiration(String expiration) {
-		this.propertiesBuilder.setExpiration(expiration);
-		return this;
-	}
-
-	public MessageBuilder setPriority(Integer priority) {
-		this.propertiesBuilder.setPriority(priority);
-		return this;
-	}
-
-	public MessageBuilder setReceivedExchange(String receivedExchange) {
-		this.propertiesBuilder.setReceivedExchange(receivedExchange);
-		return this;
-	}
-
-	public MessageBuilder setReceivedRoutingKey(String receivedRoutingKey) {
-		this.propertiesBuilder.setReceivedRoutingKey(receivedRoutingKey);
-		return this;
-	}
-
-	public MessageBuilder setRedelivered(Boolean redelivered) {
-		this.propertiesBuilder.setRedelivered(redelivered);
-		return this;
-	}
-
-	public MessageBuilder setDeliveryTag(Long deliveryTag) {
-		this.propertiesBuilder.setDeliveryTag(deliveryTag);
-		return this;
-	}
-
-	public MessageBuilder setMessageCount(Integer messageCount) {
-		this.propertiesBuilder.setMessageCount(messageCount);
-		return this;
-	}
-
-	public MessageBuilder setHeaderIfAbsent(String key, Object value) {
-		this.propertiesBuilder.setHeaderIfAbsent(key, value);
-		return this;
-	}
-
-	public MessageBuilder setTimestampIfAbsent(Date timestamp) {
-		this.propertiesBuilder.setTimestampIfAbsent(timestamp);
-		return this;
-	}
-
-	public MessageBuilder setMessageIdIfAbsent(String messageId) {
-		this.propertiesBuilder.setMessageIdIfAbsent(messageId);
-		return this;
-	}
-
-	public MessageBuilder setUserIdIfAbsent(String userId) {
-		this.propertiesBuilder.setUserIdIfAbsent(userId);
-		return this;
-	}
-
-	public MessageBuilder setAppIdIfAbsent(String appId) {
-		this.propertiesBuilder.setAppIdIfAbsent(appId);
-		return this;
-	}
-
-	public MessageBuilder setClusterIdIfAbsent(String clusterId) {
-		this.propertiesBuilder.setClusterIdIfAbsent(clusterId);
-		return this;
-	}
-
-	public MessageBuilder setTypeIfAbsent(String type) {
-		this.propertiesBuilder.setTypeIfAbsent(type);
-		return this;
-	}
-
-	public MessageBuilder setCorrelationIdIfAbsent(byte[] correlationId) {
-		this.propertiesBuilder.setCorrelationIdIfAbsent(correlationId);
-		return this;
-	}
-
-	public MessageBuilder setReplyToIfAbsent(String replyTo) {
-		this.propertiesBuilder.setReplyToIfAbsent(replyTo);
-		return this;
-	}
-
-	public MessageBuilder setReplyToAddressIfAbsent(Address replyTo) {
-		this.propertiesBuilder.setReplyToAddressIfAbsent(replyTo);
-		return this;
-	}
-
-	public MessageBuilder setContentTypeIfAbsentOrDefault(String contentType) {
-		this.propertiesBuilder.setContentTypeIfAbsentOrDefault(contentType);
-		return this;
-	}
-
-	public MessageBuilder setContentEncodingIfAbsent(String contentEncoding) {
-		this.propertiesBuilder.setContentEncodingIfAbsent(contentEncoding);
-		return this;
-	}
-
-	public MessageBuilder setContentLengthIfAbsent(long contentLength) {
-		this.propertiesBuilder.setContentLengthIfAbsent(contentLength);
-		return this;
-	}
-
-	public MessageBuilder setDeliveryModeIfAbsentOrDefault(MessageDeliveryMode deliveryMode) {
-		this.propertiesBuilder.setDeliveryModeIfAbsentOrDefault(deliveryMode);
-		return this;
-	}
-
-	public MessageBuilder setExpirationIfAbsent(String expiration) {
-		this.propertiesBuilder.setExpirationIfAbsent(expiration);
-		return this;
-	}
-
-	public MessageBuilder setPriorityIfAbsentOrDefault(Integer priority) {
-		this.propertiesBuilder.setPriorityIfAbsentOrDefault(priority);
-		return this;
-	}
-
-	public MessageBuilder setReceivedExchangeIfAbsent(String receivedExchange) {
-		this.propertiesBuilder.setReceivedExchangeIfAbsent(receivedExchange);
-		return this;
-	}
-
-	public MessageBuilder setReceivedRoutingKeyIfAbsent(String receivedRoutingKey) {
-		this.propertiesBuilder.setReceivedRoutingKeyIfAbsent(receivedRoutingKey);
-		return this;
-	}
-
-	public MessageBuilder setRedeliveredIfAbsent(Boolean redelivered) {
-		this.propertiesBuilder.setRedeliveredIfAbsent(redelivered);
-		return this;
-	}
-
-	public MessageBuilder setDeliveryTagIfAbsent(Long deliveryTag) {
-		this.propertiesBuilder.setDeliveryTagIfAbsent(deliveryTag);
-		return this;
-	}
-
-	public MessageBuilder setMessageCountIfAbsent(Integer messageCount) {
-		this.propertiesBuilder.setMessageCountIfAbsent(messageCount);
-		return this;
-	}
-
-	public MessageBuilder copyProperties(MessageProperties properties) {
-		this.propertiesBuilder.copyProperties(properties);
-		return this;
-	}
-
-	public MessageBuilder copyHeaders(Map<String, Object> headers) {
-		this.propertiesBuilder.copyHeaders(headers);
-		return this;
-	}
-
-	public MessageBuilder copyHeadersIfAbsent(Map<String, Object> headers) {
-		this.propertiesBuilder.copyHeadersIfAbsent(headers);
-		return this;
-	}
-
-	public MessageBuilder removeHeader(String key) {
-		this.propertiesBuilder.removeHeader(key);
-		return this;
-	}
-
-	public MessageBuilder removeHeaders() {
-		this.propertiesBuilder.removeHeaders();
-		return this;
-	}
-
-	/*
-	 * End delegate methods
-	 */
-
+	@Override
 	public Message build() {
-		return new Message(this.body, this.propertiesBuilder.build());
+		return new Message(this.body, this.buildProperties());
 	}
 
 }
--- spring-amqp/src/main/java/org/springframework/amqp/core/MessagePropertiesBuilder.java
+++ spring-amqp/src/main/java/org/springframework/amqp/core/MessagePropertiesBuilder.java
@@ -15,23 +15,14 @@
  */
 package org.springframework.amqp.core;
 
-import java.util.Date;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.springframework.beans.BeanUtils;
-
 /**
- * Builds a Spring AMQP {@link MessageProperties} object using a
- * fluent API.
+ * Builds a Spring AMQP MessageProperties object using a fluent API.
  *
  * @author Gary Russell
  * @since 1.3
  *
  */
-public final class MessagePropertiesBuilder {
-
-	private MessageProperties properties = new MessageProperties();
+public class MessagePropertiesBuilder extends MessageBuilderSupport<MessageProperties> {
 
 	/**
 	 * Returns a builder with an initial set of properties.
@@ -62,305 +53,22 @@ public final class MessagePropertiesBuilder {
 	}
 
 	private MessagePropertiesBuilder() {
+		super();
 	}
 
 	private MessagePropertiesBuilder(MessageProperties properties) {
-		this.properties = properties;
+		super(properties);
 	}
 
-	public MessagePropertiesBuilder setHeader(String key, Object value) {
-		this.properties.setHeader(key, value);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setTimestamp(Date timestamp) {
-		this.properties.setTimestamp(timestamp);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setMessageId(String messageId) {
-		this.properties.setMessageId(messageId);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setUserId(String userId) {
-		this.properties.setUserId(userId);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setAppId(String appId) {
-		this.properties.setAppId(appId);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setClusterId(String clusterId) {
-		this.properties.setClusterId(clusterId);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setType(String type) {
-		this.properties.setType(type);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setCorrelationId(byte[] correlationId) {
-		this.properties.setCorrelationId(correlationId);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setReplyTo(String replyTo) {
-		this.properties.setReplyTo(replyTo);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setReplyToAddress(Address replyTo) {
-		this.properties.setReplyToAddress(replyTo);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setContentType(String contentType) {
-		this.properties.setContentType(contentType);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setContentEncoding(String contentEncoding) {
-		this.properties.setContentEncoding(contentEncoding);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setContentLength(long contentLength) {
-		this.properties.setContentLength(contentLength);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setDeliveryMode(MessageDeliveryMode deliveryMode) {
-		this.properties.setDeliveryMode(deliveryMode);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setExpiration(String expiration) {
-		this.properties.setExpiration(expiration);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setPriority(Integer priority) {
-		this.properties.setPriority(priority);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setReceivedExchange(String receivedExchange) {
-		this.properties.setReceivedExchange(receivedExchange);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setReceivedRoutingKey(String receivedRoutingKey) {
-		this.properties.setReceivedRoutingKey(receivedRoutingKey);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setRedelivered(Boolean redelivered) {
-		this.properties.setRedelivered(redelivered);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setDeliveryTag(Long deliveryTag) {
-		this.properties.setDeliveryTag(deliveryTag);
-		return this;
-	}
-
-	public MessagePropertiesBuilder setMessageCount(Integer messageCount) {
-		this.properties.setMessageCount(messageCount);
-		return this;
-	}
-
-	/*
-	 * *ifAbsent variants...
-	 */
-
-	public MessagePropertiesBuilder setHeaderIfAbsent(String key, Object value) {
-		if (properties.getHeaders().get(key) == null) {
-			this.properties.setHeader(key, value);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setTimestampIfAbsent(Date timestamp) {
-		if (this.properties.getTimestamp() == null) {
-			this.properties.setTimestamp(timestamp);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setMessageIdIfAbsent(String messageId) {
-		if (this.properties.getMessageId() == null) {
-			this.properties.setMessageId(messageId);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setUserIdIfAbsent(String userId) {
-		if (this.properties.getUserId() == null) {
-			this.properties.setUserId(userId);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setAppIdIfAbsent(String appId) {
-		if (this.properties.getAppId() == null) {
-			this.properties.setAppId(appId);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setClusterIdIfAbsent(String clusterId) {
-		if (this.properties.getClusterId() == null) {
-			this.properties.setClusterId(clusterId);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setTypeIfAbsent(String type) {
-		if (this.properties.getType() == null) {
-			this.properties.setType(type);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setCorrelationIdIfAbsent(byte[] correlationId) {
-		if (this.properties.getCorrelationId() == null) {
-			this.properties.setCorrelationId(correlationId);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setReplyToIfAbsent(String replyTo) {
-		if (this.properties.getReplyTo() == null) {
-			this.properties.setReplyTo(replyTo);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setReplyToAddressIfAbsent(Address replyTo) {
-		if (this.properties.getReplyToAddress() == null) {
-			this.properties.setReplyToAddress(replyTo);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setContentTypeIfAbsentOrDefault(String contentType) {
-		if (this.properties.getContentType() == null
-				|| this.properties.getContentType() == MessageProperties.DEFAULT_CONTENT_TYPE) {
-			this.properties.setContentType(contentType);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setContentEncodingIfAbsent(String contentEncoding) {
-		if (this.properties.getContentEncoding() == null) {
-			this.properties.setContentEncoding(contentEncoding);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setContentLengthIfAbsent(long contentLength) {
-		if (this.properties.getContentLength() == null) {
-			this.properties.setContentLength(contentLength);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setDeliveryModeIfAbsentOrDefault(MessageDeliveryMode deliveryMode) {
-		if (this.properties.getDeliveryMode() == null
-				|| this.properties.getDeliveryMode() == MessageProperties.DEFAULT_DELIVERY_MODE) {
-			this.properties.setDeliveryMode(deliveryMode);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setExpirationIfAbsent(String expiration) {
-		if (this.properties.getExpiration() == null) {
-			this.properties.setExpiration(expiration);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setPriorityIfAbsentOrDefault(Integer priority) {
-		if (this.properties.getPriority() == null
-				|| this.properties.getPriority() == MessageProperties.DEFAULT_PRIORITY) {
-			this.properties.setPriority(priority);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setReceivedExchangeIfAbsent(String receivedExchange) {
-		if (this.properties.getReceivedExchange() == null) {
-			this.properties.setReceivedExchange(receivedExchange);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setReceivedRoutingKeyIfAbsent(String receivedRoutingKey) {
-		if (this.properties.getReceivedRoutingKey() == null) {
-			this.properties.setReceivedRoutingKey(receivedRoutingKey);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setRedeliveredIfAbsent(Boolean redelivered) {
-		if (this.properties.isRedelivered() == null) {
-			this.properties.setRedelivered(redelivered);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setDeliveryTagIfAbsent(Long deliveryTag) {
-		if (this.properties.getDeliveryTag() == null) {
-			this.properties.setDeliveryTag(deliveryTag);
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder setMessageCountIfAbsent(Integer messageCount) {
-		if (this.properties.getMessageCount() == null) {
-			this.properties.setMessageCount(messageCount);
-		}
-		return this;
-	}
-
+	@Override
 	public MessagePropertiesBuilder copyProperties(MessageProperties properties) {
-		BeanUtils.copyProperties(properties, this.properties);
-		// Special handling of replyTo needed because the format depends on how it was set
-		this.properties.setReplyTo(properties.getReplyTo());
-		this.properties.getHeaders().putAll(properties.getHeaders());
+		super.copyProperties(properties);
 		return this;
 	}
 
-	public MessagePropertiesBuilder copyHeaders(Map<String, Object> headers) {
-		this.properties.getHeaders().putAll(headers);
-		return this;
-	}
-
-	public MessagePropertiesBuilder copyHeadersIfAbsent(Map<String, Object> headers) {
-		Map<String, Object> existingHeaders = this.properties.getHeaders();
-		for (Entry<String, Object> entry : headers.entrySet()) {
-			if (!existingHeaders.containsKey(entry.getKey())) {
-				existingHeaders.put(entry.getKey(), entry.getValue());
-			}
-		}
-		return this;
-	}
-
-	public MessagePropertiesBuilder removeHeader(String key) {
-		this.properties.getHeaders().remove(key);
-		return this;
-	}
-
-	public MessagePropertiesBuilder removeHeaders() {
-		this.properties.getHeaders().clear();
-		return this;
-	}
-
+	@Override
 	public MessageProperties build() {
-		return this.properties;
+		return this.buildProperties();
 	}
+
 }
