ca14e276c641d2f0fc9b2b0da3ff9703cdd177f5
Gary Russell <grussell@pivotal.io>
2015-01-05 13:30:34 -0500

    AMQP-463: Fix Publisher Confirms with JRockit
    JIRA: https://jira.spring.io/browse/AMQP-463
    
    JRockit throws an `IllegalArgumentException` if a
    `TreeMap$Entry` is used after removal via the `Iterator`.
    
    The `PublishSubscribeChannelImpl` uses this technique.
    
    Obtain the entry value before removing the entry.
    
    Add a test to reproduce the problem with the JRockit JVM.
    
    Currently sends 10k messages across 100 threads; also tested
    with 1m messages across 100 threads with no lost confirms.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/support/PublisherCallbackChannelImpl.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/support/PublisherCallbackChannelImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -723,8 +723,9 @@ public class PublisherCallbackChannelImpl
 						Iterator<Entry<Long, PendingConfirm>> iterator = confirms.entrySet().iterator();
 						while (iterator.hasNext()) {
 							Entry<Long, PendingConfirm> entry = iterator.next();
+							PendingConfirm value = entry.getValue();
 							iterator.remove();
-							doHandleConfirm(ack, involvedListener, entry.getValue());
+							doHandleConfirm(ack, involvedListener, value);
 						}
 					}
 				}
