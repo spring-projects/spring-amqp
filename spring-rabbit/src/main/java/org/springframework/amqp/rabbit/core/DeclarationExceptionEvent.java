930bddea141f84224a24803d10d8f79a0cc0dada
Gary Russell <grussell@pivotal.io>
2016-01-29 17:33:52 -0500

    AMQP-548, AMQP-567: Admin Declaration Failures
    JIRA: https://jira.spring.io/browse/AMQP-548
    JIRA: https://jira.spring.io/browse/AMQP-567
    
    - `ignoreDeclarationFailures` should take effect for all exceptions
    - publish events for declaration failures
    
    Fix typo in the Doc

---
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/DeclarationExceptionEvent.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.amqp.rabbit.core;
+
+import org.springframework.amqp.core.Declarable;
+
+/**
+ * Application event published when a declaration exception occurs.
+ *
+ * @author Gary Russell
+ * @since 1.6
+ *
+ */
+public class DeclarationExceptionEvent extends RabbitAdminEvent {
+
+	private static final long serialVersionUID = -8367796410619780665L;
+
+	private final Declarable declarable;
+
+	private final Throwable throwable;
+
+	public DeclarationExceptionEvent(RabbitAdmin source, Declarable declarable, Throwable t) {
+		super(source);
+		this.declarable = declarable;
+		this.throwable = t;
+	}
+
+	/**
+	 * @return the declarable - if null, we were declaring a broker-named queue.
+	 */
+	public Declarable getDeclarable() {
+		return declarable;
+	}
+
+	/**
+	 * @return the throwable.
+	 */
+	public Throwable getThrowable() {
+		return throwable;
+	}
+
+	@Override
+	public String toString() {
+		return "DeclarationExceptionEvent [declarable=" + declarable + ", throwable=" + throwable + ", source="
+				+ getSource() + "]";
+	}
+
+}
