e3ffacd5ce569d14b2a7c03cb635895b8bb8be53
Gary Russell <grussell@pivotal.io>
2016-02-01 19:14:19 -0500

    Add Properties Test

--- spring-rabbit-ext/src/test/java/org/springframework/amqp/rabbit/log4j2/test/AmqpAppenderTests.java
+++ spring-rabbit-ext/src/test/java/org/springframework/amqp/rabbit/log4j2/test/AmqpAppenderTests.java
@@ -14,7 +14,11 @@
 package org.springframework.amqp.rabbit.log4j2.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,11 +28,14 @@ import org.junit.Test;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.log4j2.AmqpAppender;
 import org.springframework.amqp.rabbit.test.BrokerRunning;
+import org.springframework.amqp.utils.test.TestUtils;
 
 /**
  * @author Gary Russell
@@ -57,5 +64,42 @@ public class AmqpAppenderTests {
 		assertNotNull(received);
 		assertEquals("testAppId.foo.INFO", received.getMessageProperties().getReceivedRoutingKey());
 	}
+
+	@Test
+	public void testProperties() {
+		Log logger = LogFactory.getLog("foo");
+		AmqpAppender appender = (AmqpAppender) TestUtils
+				.getPropertyValue(logger, "logger.context.configuration.appenders", Map.class).get("rabbitmq");
+		Object manager = TestUtils.getPropertyValue(appender, "manager");
+//		<RabbitMQ name="rabbitmq"
+//				host="localhost" port="5672" user="guest" password="guest" virtualHost="/"
+//				exchange="log4j2Test" exchangeType="fanout" declareExchange="true" durable="true" autoDelete="false"
+//				applicationId="testAppId" routingKeyPattern="%X{applicationId}.%c.%p"
+//				contentType="text/plain" contentEncoding="UTF-8" generateId="true" deliveryMode="NON_PERSISTENT"
+//				charset="UTF-8"
+//				senderPoolSize="3" maxSenderRetries="5">
+//		</RabbitMQ>
+		assertEquals("localhost", TestUtils.getPropertyValue(manager, "host"));
+		assertEquals(5672, TestUtils.getPropertyValue(manager, "port"));
+		assertEquals("guest", TestUtils.getPropertyValue(manager, "username"));
+		assertEquals("guest", TestUtils.getPropertyValue(manager, "password"));
+		assertEquals("/", TestUtils.getPropertyValue(manager, "virtualHost"));
+		assertEquals("log4j2Test", TestUtils.getPropertyValue(manager, "exchangeName"));
+		assertEquals("fanout", TestUtils.getPropertyValue(manager, "exchangeType"));
+		assertTrue(TestUtils.getPropertyValue(manager, "declareExchange", Boolean.class));
+		assertTrue(TestUtils.getPropertyValue(manager, "durable", Boolean.class));
+		assertFalse(TestUtils.getPropertyValue(manager, "autoDelete", Boolean.class));
+		assertEquals("testAppId", TestUtils.getPropertyValue(manager, "applicationId"));
+		assertEquals("%X{applicationId}.%c.%p", TestUtils.getPropertyValue(manager, "routingKeyPattern"));
+		assertEquals("text/plain", TestUtils.getPropertyValue(manager, "contentType"));
+		assertEquals("UTF-8", TestUtils.getPropertyValue(manager, "contentEncoding"));
+		assertTrue(TestUtils.getPropertyValue(manager, "generateId", Boolean.class));
+		assertEquals(MessageDeliveryMode.NON_PERSISTENT, TestUtils.getPropertyValue(manager, "deliveryMode"));
+		assertEquals("UTF-8", TestUtils.getPropertyValue(manager, "contentEncoding"));
+		assertEquals(3, TestUtils.getPropertyValue(manager, "senderPoolSize"));
+		assertEquals(5, TestUtils.getPropertyValue(manager, "maxSenderRetries"));
+	}
+
+
 
 }
