770ff7a42228d696b42d67fa8b93c0f1f3b0abe8
Gary Russell <grussell@pivotal.io>
2016-05-17 10:12:28 -0400

    GH-420: AcknowledgeMode JavaDocs
    Fixes #420

--- spring-amqp/src/main/java/org/springframework/amqp/core/AcknowledgeMode.java
+++ spring-amqp/src/main/java/org/springframework/amqp/core/AcknowledgeMode.java
@@ -17,21 +17,53 @@
 package org.springframework.amqp.core;
 
 /**
+ * Acknowledgment modes supported by the listener container.
+ *
  * @author Dave Syer
  *
  */
 public enum AcknowledgeMode {
 
-	NONE, MANUAL, AUTO;
+	/**
+	 * No acks - {@code autoAck=true} in {@code Channe.basicConsume()}.
+	 */
+	NONE,
 
+	/**
+	 * Manual acks - user must ack/nack via a channel aware listener.
+	 */
+	MANUAL,
+
+	/**
+	 * Auto - the container will issue the ack/nack based on whether
+	 * the listener returns normally, or throws an exception.
+	 * <p><em>Do not confuse with RabbitMQ {@code autoAck} which is
+	 * represented by {@link #NONE} here.
+	 */
+	AUTO;
+
+	/**
+	 * Return if transactions are allowed - if the mode is {@link #AUTO} or
+	 * {@link #MANUAL}.
+	 * @return true if transactions are allowed.
+	 */
 	public boolean isTransactionAllowed() {
 		return this == AUTO || this == MANUAL;
 	}
 
+	/**
+	 * Return if the mode is {@link #NONE} (which is called {@code autoAck}
+	 * in RabbitMQ).
+	 * @return true if the mode is {@link #NONE}.
+	 */
 	public boolean isAutoAck() {
 		return this == NONE;
 	}
 
+	/**
+	 * Return true if the mode is {@link #MANUAL}.
+	 * @return true if manual.
+	 */
 	public boolean isManual() {
 		return this == MANUAL;
 	}
