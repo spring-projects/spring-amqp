1c8ca3a4673d094a00b722177b0393a1c860a76f
Gary Russell <grussell@pivotal.io>
2016-10-17 12:38:52 -0400

    AMQP-659: @RabbitListener Annotation Improvements
    JIRA: https://jira.spring.io/browse/AMQP-659
    
    - add `@AliasFor` to `@Queue` and `@Exchange` annotation `value()`
    - Support simple queue declaration (no binding)
    
    * Fix typos in logs messages

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/annotation/RabbitListener.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/annotation/RabbitListener.java
@@ -120,11 +120,26 @@ public @interface RabbitListener {
 	 * The queues for this listener.
 	 * The entries can be 'queue name', 'property-placeholder keys' or 'expressions'.
 	 * Expression must be resolved to the queue name or {@code Queue} object.
-	 * Mutually exclusive with {@link #bindings()}
+	 * The queue(s) must exist, or be otherwise defined elsewhere as a bean(s) with
+	 * a {@link org.springframework.amqp.rabbit.core.RabbitAdmin} in the application
+	 * context.
+	 * Mutually exclusive with {@link #bindings()} and {@link #queuesToDeclare()}.
 	 * @return the queue names or expressions (SpEL) to listen to from target
-	 * {@link org.springframework.amqp.rabbit.listener.MessageListenerContainer}.
+	 * @see org.springframework.amqp.rabbit.listener.MessageListenerContainer
 	 */
 	String[] queues() default {};
+
+	/**
+	 * The queues for this listener.
+	 * If there is a {@link org.springframework.amqp.rabbit.core.RabbitAdmin} in the
+	 * application context, the queue will be declared on the broker with default
+	 * binding (default exchange with the queue name as the routing key).
+	 * Mutually exclusive with {@link #bindings()} and {@link #queues()}.
+	 * @return the queue(s) to declare.
+	 * @see org.springframework.amqp.rabbit.listener.MessageListenerContainer
+	 * @since 2.0
+	 */
+	Queue[] queuesToDeclare() default {};
 
 	/**
 	 * When {@code true}, a single consumer in the container will have exclusive use of the
@@ -148,14 +163,16 @@ public @interface RabbitListener {
 	 * queues and those queues are configured for conditional declaration. This
 	 * is the admin that will (re)declare those queues when the container is
 	 * (re)started. See the reference documentation for more information.
-	  @return the {@link org.springframework.amqp.rabbit.core.RabbitAdmin} bean name.
+	 * @return the {@link org.springframework.amqp.rabbit.core.RabbitAdmin} bean name.
 	 */
 	String admin() default "";
 
 	/**
 	 * Array of {@link QueueBinding}s providing the listener's queue names, together
 	 * with the exchange and optional binding information.
+	 * Mutually exclusive with {@link #queues()} and {@link #queuesToDeclare()}.
 	 * @return the bindings.
+	 * @see org.springframework.amqp.rabbit.listener.MessageListenerContainer
 	 * @since 1.5
 	 */
 	QueueBinding[] bindings() default {};
