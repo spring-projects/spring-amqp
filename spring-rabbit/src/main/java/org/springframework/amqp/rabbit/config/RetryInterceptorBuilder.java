601f305568fd19bb3af4b181d6ce7b3c81e264af
Gary Russell <grussell@pivotal.io>
2016-11-01 16:23:15 -0400

    Polishing
    Avoid empty CTORs to avoid Sonar complaints.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/RetryInterceptorBuilder.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/RetryInterceptorBuilder.java
@@ -292,6 +292,7 @@ public abstract class RetryInterceptorBuilder<T extends MethodInterceptor> {
 				new StatelessRetryOperationsInterceptorFactoryBean();
 
 		StatelessRetryInterceptorBuilder() {
+			super();
 		}
 
 		@Override
