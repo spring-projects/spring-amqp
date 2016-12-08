9e84e6ff34a8787f7f133bea6dcaf1b2a2e00c37
Gary Russell <grussell@pivotal.io>
2016-12-08 17:35:23 -0500

    AMQP-687: Fix Conditional Rollback
    JIRA: https://jira.spring.io/browse/AMQP-687
    
    When using a rule-based transaction attribute with a rollback/no-rollback rule,
    the rule is ignored when providing the transaction attribute directly to the container
    rather than via a transaction advice,
    
    Also, user exceptions are always wrapped in a `ListenerExecutionFailedException` which
    limits the ability to configure rules, even when using an advice.
    
    - Honor the attribute `rollBackOn()` mechanism within the container
    - Add `ListenerFailedRuleBasedTransactionAttribute` to allow the user to configure rules based on the actual exception.
    
    Add docs.
    
    AMQP-686: Handle ImmediateAcknowledgeAmqpException
    
    JIRA: https://jira.spring.io/browse/AMQP-686
    
    Previously, `ImmediateAcknowledgeAmqpException` only worked if thrown by
    an error handler, not a listener.
    
    Examine the cause chain to see if this exception appears, and handle accordingly.
    
    An `AmqpRejectAndDontRequeueException` found first stops the chain traversal.
    
    Also clarify the javadocs to make it clear the current transaction is ended
    immediately, not all message delivery.
    
    checkstyle
    
    Polishing and Fixes
    
    While polishing for PR comments, I found some more problems (existing code).
    
    When using an external transaction manager, txRollback() was called twice, once
    within the transaction and once by the after completion in the transaction synchronization.
    
    This was benign because the delivery tags were not registered, but incorrect.
    
    Now, the delivery tag is always registered and the rollback done by the synchronization.
    
    Added more tests and added tests for the local transaction scenario.
    
    Add missing .this
    
    Polishing
    
    Conflicts:
    	spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/AbstractMessageListenerContainer.java
    	spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/DirectMessageListenerContainer.java
    	spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/SimpleMessageListenerContainer.java
    	spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/ExternalTxManagerSMLCTests.java
    	spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/ExternalTxManagerTests.java
    	spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/LocallyTransactedTests.java
    	src/reference/asciidoc/whats-new.adoc
    
    Resolved.

---
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/transaction/ListenerFailedRuleBasedTransactionAttribute.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.amqp.rabbit.transaction;
+
+import org.springframework.amqp.rabbit.listener.exception.ListenerExecutionFailedException;
+import org.springframework.transaction.interceptor.RuleBasedTransactionAttribute;
+
+/**
+ * Subclass of {@link RuleBasedTransactionAttribute} that is aware that
+ * listener exceptions are wrapped in {@link ListenerExecutionFailedException}s.
+ * Allows users to control rollback based on the actual cause.
+ *
+ * @author Gary Russell
+ * @since 1.6.6
+ *
+ */
+@SuppressWarnings("serial")
+public class ListenerFailedRuleBasedTransactionAttribute extends RuleBasedTransactionAttribute {
+
+	@Override
+	public boolean rollbackOn(Throwable ex) {
+		if (ex instanceof ListenerExecutionFailedException) {
+			return super.rollbackOn(ex.getCause());
+		}
+		else {
+			return super.rollbackOn(ex);
+		}
+	}
+
+}
