1ebbcfaa21426d1cf7f401df8dd35125b0528585
Artem Bilan <abilan@pivotal.io>
2017-01-30 19:34:28 -0500

    AMQP-705: Properly use `Assert` class from SF
    JIRA: https://jira.spring.io/browse/AMQP-705
    
    Fixes spring-projects/spring-amqp#554
    
    **Cherry-pick to 1.7.x, 1.6.x**
    
    (cherry picked from commit e5cb8b5)

--- spring-amqp/src/main/java/org/springframework/amqp/support/postprocessor/UnzipPostProcessor.java
+++ spring-amqp/src/main/java/org/springframework/amqp/support/postprocessor/UnzipPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2016 the original author or authors.
+ * Copyright 2014-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,8 @@ public class UnzipPostProcessor extends AbstractDecompressingPostProcessor {
 	protected InputStream getDecompressorStream(InputStream zipped) throws IOException {
 		ZipInputStream zipper = new ZipInputStream(zipped);
 		ZipEntry entry = zipper.getNextEntry();
-		Assert.state("amqp".equals(entry.getName()));
+		String entryName = entry.getName();
+		Assert.state("amqp".equals(entryName), "Zip 'entryName' must be equal to 'amqp'");
 		return zipper;
 	}
 
--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitMessagingTemplate.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitMessagingTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,7 +58,7 @@ public class RabbitMessagingTemplate extends AbstractMessagingTemplate<String>
 	 * @param rabbitTemplate the template.
 	 */
 	public RabbitMessagingTemplate(RabbitTemplate rabbitTemplate) {
-		Assert.notNull("RabbitTemplate must not be null");
+		Assert.notNull(rabbitTemplate, "'rabbitTemplate' must not be null");
 		this.rabbitTemplate = rabbitTemplate;
 	}
 
