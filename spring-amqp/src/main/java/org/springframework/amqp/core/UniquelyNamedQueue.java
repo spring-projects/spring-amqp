3122446e966c35e1d7b992404a210c77a5d2da74
Gary Russell <grussell@pivotal.io>
2017-03-06 10:32:26 -0500

    AMQP-720: Deprecate UniquelyNamedQueue
    JIRA: https://jira.spring.io/browse/AMQP-720

--- spring-amqp/src/main/java/org/springframework/amqp/core/UniquelyNamedQueue.java
+++ spring-amqp/src/main/java/org/springframework/amqp/core/UniquelyNamedQueue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,8 +22,11 @@ import java.util.UUID;
  * Create a queue with a unique name.
  *
  * @author Mark Pollack
+ * @author Gary Russell
+ * @deprecated - this class will be removed in 2.0.
  *
  */
+@Deprecated
 public class UniquelyNamedQueue extends Queue {
 
 	/**
