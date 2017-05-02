4d0291ca67f5fef3e18aeac7ac41811a23a17ed1
Gary Russell <grussell@pivotal.io>
2017-05-02 11:33:42 -0400

    AMQP-738: DMLC: Add messagesPerAck
    JIRA: https://jira.spring.io/browse/AMQP-738
    
    Using a `DirectMessageListenerContainer`, support sending acks in batches
    to reduce network traffic in high volume environments.
    
    Javadoc Fix
    
    More Javadoc Polishing
    
    Polishing - PR Comments

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/DirectMessageListenerContainerMockTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/DirectMessageListenerContainerMockTests.java
@@ -22,16 +22,20 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -41,8 +45,10 @@ import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.Envelope;
 import com.rabbitmq.client.impl.recovery.AutorecoveringChannel;
 
 /**
@@ -94,6 +100,106 @@ public class DirectMessageListenerContainerMockTests {
 		isOpen.set(false);
 		assertTrue(latch2.await(10, TimeUnit.SECONDS));
 		container.stop();
+	}
+
+	@Test
+	public void testDeferredAcks() throws Exception {
+		ConnectionFactory connectionFactory = mock(ConnectionFactory.class);
+		Connection connection = mock(Connection.class);
+		ChannelProxy channel = mock(ChannelProxy.class);
+		Channel rabbitChannel = mock(AutorecoveringChannel.class);
+		given(channel.getTargetChannel()).willReturn(rabbitChannel);
+
+		given(connectionFactory.createConnection()).willReturn(connection);
+		given(connection.createChannel(anyBoolean())).willReturn(channel);
+		given(channel.isOpen()).willReturn(true);
+		given(channel.queueDeclarePassive(Mockito.anyString()))
+				.willAnswer(invocation -> mock(AMQP.Queue.DeclareOk.class));
+		final AtomicReference<Consumer> consumer = new AtomicReference<>();
+		final CountDownLatch latch1 = new CountDownLatch(1);
+		willAnswer(i -> {
+			consumer.set(i.getArgument(6));
+			consumer.get().handleConsumeOk("consumerTag");
+			latch1.countDown();
+			return "consumerTag";
+		})
+		.given(channel).basicConsume(anyString(), anyBoolean(), anyString(), anyBoolean(), anyBoolean(),
+						anyMap(), any(Consumer.class));
+
+		final AtomicInteger qos = new AtomicInteger();
+		willAnswer(i -> {
+			qos.set(i.getArgument(0));
+			return null;
+		}).given(channel).basicQos(anyInt());
+		final CountDownLatch latch2 = new CountDownLatch(2);
+		final CountDownLatch latch3 = new CountDownLatch(1);
+		willAnswer(i -> {
+			if (i.getArgument(0).equals(10L) || i.getArgument(0).equals(16L)) {
+				latch2.countDown();
+			}
+			else if (i.getArgument(0).equals(17L)) {
+				latch3.countDown();
+			}
+			return null;
+		}).given(channel).basicAck(anyLong(), anyBoolean());
+		final CountDownLatch latch4 = new CountDownLatch(1);
+		willAnswer(i -> {
+			latch4.countDown();
+			return null;
+		}).given(channel).basicNack(19L, true, true);
+
+		DirectMessageListenerContainer container = new DirectMessageListenerContainer(connectionFactory);
+		container.setQueueNames("test");
+		container.setPrefetchCount(2);
+		container.setMonitorInterval(100);
+		container.setMessagesPerAck(10);
+		container.setAckTimeout(100);
+		container.setMessageListener(m -> {
+			if (m.getMessageProperties().getDeliveryTag() == 19L) {
+				throw new RuntimeException("TestNackAndPendingAcks");
+			}
+		});
+		container.afterPropertiesSet();
+		container.start();
+
+		assertTrue(latch1.await(10, TimeUnit.SECONDS));
+		assertThat(qos.get(), equalTo(2));
+		BasicProperties props = new BasicProperties();
+		byte[] body = new byte[1];
+		for (long i = 1; i < 16; i++) {
+			consumer.get().handleDelivery("consumerTag", envelope(i), props, body);
+		}
+		Thread.sleep(200);
+		consumer.get().handleDelivery("consumerTag", envelope(16), props, body);
+		// should get 2 acks #10 and #6 (timeout)
+		assertTrue(latch2.await(10, TimeUnit.SECONDS));
+		consumer.get().handleDelivery("consumerTag", envelope(17), props, body);
+		verify(channel).basicAck(10L, true);
+		verify(channel).basicAck(16L, true);
+		assertTrue(latch3.await(10, TimeUnit.SECONDS));
+		// monitor task timeout
+		verify(channel).basicAck(17L, true);
+		consumer.get().handleDelivery("consumerTag", envelope(18), props, body);
+		consumer.get().handleDelivery("consumerTag", envelope(19), props, body);
+		assertTrue(latch4.await(10, TimeUnit.SECONDS));
+		// pending acks before nack
+		verify(channel).basicAck(18L, true);
+		verify(channel).basicNack(19L, true, true);
+		consumer.get().handleDelivery("consumerTag", envelope(20), props, body);
+		final CountDownLatch latch5 = new CountDownLatch(1);
+		willAnswer(i -> {
+			consumer.get().handleCancelOk("consumerTag");
+			latch5.countDown();
+			return null;
+		}).given(channel).basicCancel("consumerTag");
+		Executors.newSingleThreadExecutor().execute(container::stop);
+		assertTrue(latch5.await(10, TimeUnit.SECONDS));
+		// pending acks on stop
+		verify(channel).basicAck(20L, true);
+	}
+
+	private Envelope envelope(long tag) {
+		return new Envelope(tag,  false, "", "");
 	}
 
 }
