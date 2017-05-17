5e33f525248b724a70ccdd3d03bfcf4bd3bdebf3
Artem Bilan <abilan@pivotal.io>
2017-05-17 10:50:39 -0400

    Increase timeouts for ContainerInitTests
    https://build.spring.io/browse/AMQP-MEIGHT-1066/

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/ContainerInitializationTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/ContainerInitializationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,7 +92,7 @@ public class ContainerInitializationTests {
 		admin.declareQueue(new Queue(TEST_MISMATCH, false, false, true));
 		SimpleMessageListenerContainer container = context.getBean(SimpleMessageListenerContainer.class);
 		int n = 0;
-		while (n++ < 100 && container.isRunning()) {
+		while (n++ < 200 && container.isRunning()) {
 			Thread.sleep(100);
 		}
 		assertFalse(container.isRunning());
@@ -107,7 +107,7 @@ public class ContainerInitializationTests {
 		admin.declareQueue(new Queue(TEST_MISMATCH, false, false, true));
 		SimpleMessageListenerContainer container = context.getBean(SimpleMessageListenerContainer.class);
 		int n = 0;
-		while (n++ < 100 && container.isRunning()) {
+		while (n++ < 200 && container.isRunning()) {
 			Thread.sleep(100);
 		}
 		assertFalse(container.isRunning());
