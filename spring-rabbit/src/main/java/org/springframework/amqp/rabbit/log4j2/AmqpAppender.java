7349001c39d6dd16e435dd595f7cdabe9a6d9743
Gary Russell <grussell@pivotal.io>
2017-05-25 12:04:50 -0400

    Revert AMQP-743
    JIRA: https://jira.spring.io/browse/AMQP-743
    
    This reverts commit cd50ceafd7957499214126524c154d658c448ec9.
    
    The abstract appender doesn't have a stop() method in this version.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/log4j2/AmqpAppender.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/log4j2/AmqpAppender.java
@@ -308,12 +308,6 @@ public class AmqpAppender extends AbstractAppender {
 		}
 	}
 
-	@Override
-	protected boolean stop(long timeout, TimeUnit timeUnit, boolean changeLifeCycleState) {
-		boolean stopped = super.stop(timeout, timeUnit, changeLifeCycleState);
-		return stopped & this.manager.stop(timeout, timeUnit);
-	}
-
 	/**
 	 * Helper class to actually send LoggingEvents asynchronously.
 	 */
