8d73987a82e8e268043bda90b077aa5329d0df0a
Gary Russell <grussell@pivotal.io>
2017-08-25 10:06:06 -0400

    AMQP-765: Suppress Exception on Deferred Close
    JIRA: https://jira.spring.io/browse/AMQP-765
    
    When confirms/returns are enabled we defer the close; suppress
    the logged exception if a normal `ShutdownSignalException` is thrown.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/CachingConnectionFactory.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/CachingConnectionFactory.java
@@ -1063,6 +1063,11 @@ public class CachingConnectionFactory extends AbstractConnectionFactory
 					catch (IOException e3) { }
 					catch (AlreadyClosedException e4) { }
 					catch (TimeoutException e5) { }
+					catch (ShutdownSignalException e6) {
+						if (!RabbitUtils.isNormalShutdown(e6)) {
+							logger.debug("Unexpected exception on deferred close", e6);
+						}
+					}
 				}
 			});
 		}
