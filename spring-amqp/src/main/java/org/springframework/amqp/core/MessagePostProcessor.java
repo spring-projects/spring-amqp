cc138bbde8fb0db4d3901045b08dc07a954a6d61
Gary Russell <grussell@pivotal.io>
2017-11-28 10:50:54 -0500

    AMQP-789: MPP Javadoc polishing
    JIRA: https://jira.spring.io/browse/AMQP-789

--- spring-amqp/src/main/java/org/springframework/amqp/core/MessagePostProcessor.java
+++ spring-amqp/src/main/java/org/springframework/amqp/core/MessagePostProcessor.java
@@ -20,12 +20,21 @@ import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.support.Correlation;
 
 /**
- * To be used with the send method of Amqp template classes (such as RabbitTemplate)
- * that convert an object to a message.
- * It allows for further modification of the message after it has been processed
- * by the converter. This is useful for setting of Headers and Properties.
+ * Used in several places in the framework, such as
+ * {@code AmqpTemplate#convertAndSend(Object, MessagePostProcessor)} where it can be used
+ * to add/modify headers or properties after the message conversion has been performed. It
+ * also can be used to modify inbound messages when receiving messages in listener
+ * containers and {@code AmqpTemplate}s.
  *
- * <p>This often as an anonymous class within a method implementation.
+ * <p>
+ * It is a {@link FunctionalInterface} and is often used as a lambda:
+ * <pre class="code">
+ * amqpTemplate.convertAndSend(routingKey, m -&gt; {
+ *     m.getMessageProperties().setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+ *     return m;
+ * });
+ * </pre>
+ *
  * @author Mark Pollack
  * @author Gary Russell
  */
