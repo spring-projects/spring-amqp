489df246b21268f1f15b134ab16b4d0b27b48334
Mark Norkin <markinnork7@gmail.com>
2018-02-01 23:35:46 +0200

    Access directReplyToContainers map only via lock
    remove double checking for null container

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitTemplate.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitTemplate.java
@@ -1627,25 +1627,23 @@ public class RabbitTemplate extends RabbitAccessor implements BeanFactoryAware, 
 		if (this.usePublisherConnection && connectionFactory.getPublisherConnectionFactory() != null) {
 			connectionFactory = connectionFactory.getPublisherConnectionFactory();
 		}
-		DirectReplyToMessageListenerContainer container = this.directReplyToContainers.get(connectionFactory);
-		if (container == null) {
-			synchronized (this.directReplyToContainers) {
-				container = this.directReplyToContainers.get(connectionFactory);
-				if (container == null) {
-					container = new DirectReplyToMessageListenerContainer(connectionFactory);
-					container.setMessageListener(this);
-					container.setBeanName(this.beanName + "#" + this.containerInstance.getAndIncrement());
-					if (this.taskExecutor != null) {
-						container.setTaskExecutor(this.taskExecutor);
-					}
-					if (this.afterReceivePostProcessors != null) {
-						container.setAfterReceivePostProcessors(this.afterReceivePostProcessors
-								.toArray(new MessagePostProcessor[this.afterReceivePostProcessors.size()]));
-					}
-					container.start();
-					this.directReplyToContainers.put(connectionFactory, container);
-					this.replyAddress = Address.AMQ_RABBITMQ_REPLY_TO;
+		DirectReplyToMessageListenerContainer container;
+		synchronized (this.directReplyToContainers) {
+			container = this.directReplyToContainers.get(connectionFactory);
+			if (container == null) {
+				container = new DirectReplyToMessageListenerContainer(connectionFactory);
+				container.setMessageListener(this);
+				container.setBeanName(this.beanName + "#" + this.containerInstance.getAndIncrement());
+				if (this.taskExecutor != null) {
+					container.setTaskExecutor(this.taskExecutor);
 				}
+				if (this.afterReceivePostProcessors != null) {
+					container.setAfterReceivePostProcessors(this.afterReceivePostProcessors
+							.toArray(new MessagePostProcessor[this.afterReceivePostProcessors.size()]));
+				}
+				container.start();
+				this.directReplyToContainers.put(connectionFactory, container);
+				this.replyAddress = Address.AMQ_RABBITMQ_REPLY_TO;
 			}
 		}
 		ChannelHolder channelHolder = container.getChannelHolder();
