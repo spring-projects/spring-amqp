f3273b14af72696e568029827d468194932453ab
Mark Norkin <markinnork7@gmail.com>
2018-02-01 23:27:23 +0200

    change copyright

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitTemplate.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -135,6 +135,7 @@ import com.rabbitmq.client.GetResponse;
  * @author Gary Russell
  * @author Artem Bilan
  * @author Ernest Sadykov
+ * @author Mark Norkin
  * @since 1.0
  */
 public class RabbitTemplate extends RabbitAccessor implements BeanFactoryAware, RabbitOperations, MessageListener,
