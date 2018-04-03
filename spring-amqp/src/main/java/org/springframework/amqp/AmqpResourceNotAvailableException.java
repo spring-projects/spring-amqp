f69f54e828cadf65296f3302ceed5a00307c64d0
Artem Bilan <abilan@pivotal.io>
2018-04-03 17:38:03 -0400

    GH-723: Add AmqpResourceNotAvailableException (#737)
    * GH-723: Add AmqpResourceNotAvailableException
    
    Fixes spring-projects/spring-amqp#723
    
    To avoid an `NPE` when connection returns `null` for the
    `createChannel()` in case of `channelMax` is reached, throw newly
    introduced `AmqpResourceNotAvailableException`.
    This exception can be used in the `RetryPolicy` to retry the original
    operation after some back-off - the channel permit may be released in
    between
    
    **Cherry-pick to 2.0.x and 1.7.x**
    
    * * Fix `AbstractConnectionFactoryTests` for proper mock
    * Fix `amqp.adoc` according PR comments

---
+++ spring-amqp/src/main/java/org/springframework/amqp/AmqpResourceNotAvailableException.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.amqp;
+
+/**
+ * The {@link AmqpException} thrown when some resource can't be accessed.
+ * For example when {@code channelMax} limit is reached and connect can't
+ * create a new channel at the moment.
+ *
+ * @author Artem Bilan
+ *
+ * @since 1.7.7
+ */
+public class AmqpResourceNotAvailableException extends AmqpException {
+
+	public AmqpResourceNotAvailableException(String message) {
+		super(message);
+	}
+
+	public AmqpResourceNotAvailableException(Throwable cause) {
+		super(cause);
+	}
+
+	public AmqpResourceNotAvailableException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
