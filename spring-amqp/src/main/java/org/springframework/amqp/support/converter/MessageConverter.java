b6b4f5ba3af64f0ab078946fc8accc08e77077ba
Gary Russell <grussell@pivotal.io>
2018-11-29 16:27:17 -0500

    Remove erroneous @Nullable
    Incorrectly added to message converter.

--- spring-amqp/src/main/java/org/springframework/amqp/support/converter/MessageConverter.java
+++ spring-amqp/src/main/java/org/springframework/amqp/support/converter/MessageConverter.java
@@ -62,7 +62,6 @@ public interface MessageConverter {
 	 * @return the converted Java object
 	 * @throws MessageConversionException in case of conversion failure
 	 */
-	@Nullable
 	Object fromMessage(Message message) throws MessageConversionException;
 
 }
