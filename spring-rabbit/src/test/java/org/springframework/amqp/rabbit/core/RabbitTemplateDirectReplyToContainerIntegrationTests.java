08f54ce0ad72193eb1f635380b9a19345dcb3947
Gary Russell <grussell@pivotal.io>
2018-12-13 12:28:30 -0500

    AMQP-849: RT and DRTMLC - add ErrorHandler
    JIRA: https://jira.spring.io/browse/AMQP-849
    
    Support the configuration of an error handler for exceptions when
    delivering replies (e.g. late replies).
    
    **cherry-pick to 2.0.x with adjustments to assertJ and docs**
    
    * Polishing - PR Comments
    
    * More polishing
    
    * Replace AssertJ with regular JUnit assertions
    
    # Conflicts:
    #	spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitTemplate.java
    #	spring-rabbit/src/test/java/org/springframework/amqp/rabbit/core/RabbitTemplateDirectReplyToContainerIntegrationTests.java
    #	src/reference/asciidoc/amqp.adoc
    #	src/reference/asciidoc/whats-new.adoc

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/core/RabbitTemplateDirectReplyToContainerIntegrationTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/core/RabbitTemplateDirectReplyToContainerIntegrationTests.java
@@ -16,16 +16,32 @@
 
 package org.springframework.amqp.rabbit.core;
 
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.Test;
 
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.exception.ListenerExecutionFailedException;
 import org.springframework.amqp.utils.test.TestUtils;
+import org.springframework.util.ErrorHandler;
 
 /**
  * @author Gary Russell
@@ -46,10 +62,17 @@ public class RabbitTemplateDirectReplyToContainerIntegrationTests extends Rabbit
 
 	@SuppressWarnings("unchecked")
 	@Test
-	public void channelReleasedOnTimeout() {
+	public void channelReleasedOnTimeout() throws Exception {
 		final CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
 		RabbitTemplate template = createSendAndReceiveRabbitTemplate(connectionFactory);
 		template.setReplyTimeout(1);
+		AtomicReference<Throwable> exception = new AtomicReference<>();
+		CountDownLatch latch = new CountDownLatch(1);
+		ErrorHandler replyErrorHandler = t -> {
+			exception.set(t);
+			latch.countDown();
+		};
+		template.setReplyErrorHandler(replyErrorHandler);
 		Object reply = template.convertSendAndReceive(ROUTE, "foo");
 		assertNull(reply);
 		Object container = TestUtils.getPropertyValue(template, "directReplyToContainers", Map.class)
@@ -57,6 +80,48 @@ public class RabbitTemplateDirectReplyToContainerIntegrationTests extends Rabbit
 						? connectionFactory.getPublisherConnectionFactory()
 						: connectionFactory);
 		assertEquals(0, TestUtils.getPropertyValue(container, "inUseConsumerChannels", Map.class).size());
+		assertSame(replyErrorHandler, TestUtils.getPropertyValue(container, "errorHandler"));
+		Message replyMessage = new Message("foo".getBytes(), new MessageProperties());
+		try {
+			template.onMessage(replyMessage);
+		}
+		catch (Exception e) {
+			assertThat(e, instanceOf(AmqpRejectAndDontRequeueException.class));
+			assertEquals("No correlation header in reply", e.getMessage());
+		}
+
+		replyMessage.getMessageProperties().setCorrelationId("foo");
+
+		try {
+			template.onMessage(replyMessage);
+		}
+		catch (Exception e) {
+			assertThat(e, instanceOf(AmqpRejectAndDontRequeueException.class));
+			assertEquals("Reply received after timeout", e.getMessage());
+		}
+
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		// Set up a consumer to respond to our producer
+		executor.submit(() -> {
+			Message message = template.receive(ROUTE, 10_000);
+			assertNotNull("No message received", message);
+			template.send(message.getMessageProperties().getReplyTo(), replyMessage);
+			return message;
+		});
+		while (template.receive(ROUTE, 100) != null) {
+			// empty
+		}
+		reply = template.convertSendAndReceive(ROUTE, "foo");
+		assertNull(reply);
+		assertTrue(latch.await(10, TimeUnit.SECONDS));
+		assertThat(exception.get(), instanceOf(ListenerExecutionFailedException.class));
+		assertEquals("Reply received after timeout", exception.get().getCause().getMessage());
+		assertArrayEquals(replyMessage.getBody(),
+				((ListenerExecutionFailedException) exception.get()).getFailedMessage().getBody());
+		assertEquals(0, TestUtils.getPropertyValue(container, "inUseConsumerChannels", Map.class).size());
+		executor.shutdownNow();
+		template.stop();
+		connectionFactory.destroy();
 	}
 
 }
