a2cec56235cf972cc69ff3603795cf1d80cbff27
Gary Russell <grussell@pivotal.io>
2019-01-18 12:11:51 -0500

    GH-883: Fix regression in transaction manager
    Fixes https://github.com/spring-projects/spring-amqp/issues/883

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/transaction/RabbitTransactionManager.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/transaction/RabbitTransactionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -113,7 +113,7 @@ public class RabbitTransactionManager extends AbstractPlatformTransactionManager
 	 */
 	@Override
 	public void afterPropertiesSet() {
-		Assert.state(getConnectionFactory() == null, "Property 'connectionFactory' is required");
+		Assert.state(getConnectionFactory() != null, "Property 'connectionFactory' is required");
 	}
 
 	@Override
--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/transaction/RabbitTransactionManagerIntegrationTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/transaction/RabbitTransactionManagerIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 the original author or authors.
+ * Copyright 2011-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,6 +54,7 @@ public class RabbitTransactionManagerIntegrationTests {
 		template = new RabbitTemplate(connectionFactory);
 		template.setChannelTransacted(true);
 		RabbitTransactionManager transactionManager = new RabbitTransactionManager(connectionFactory);
+		transactionManager.afterPropertiesSet();
 		transactionTemplate = new TransactionTemplate(transactionManager);
 	}
 
