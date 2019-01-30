36b2378ce9f063624556421e769832b665aba045
Sud Ramasamy <sudhakar.ramasamy@mercurygate.com>
2019-01-30 15:19:38 -0500

    Update copyright year and author for affected classes

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/DirectRabbitListenerContainerFactory.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/DirectRabbitListenerContainerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2016-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import org.springframework.scheduling.TaskScheduler;
  * implementation to build a regular {@link DirectMessageListenerContainer}.
  *
  * @author Gary Russell
+ * @author Sud Ramasamy
  * @since 2.0
  */
 public class DirectRabbitListenerContainerFactory
