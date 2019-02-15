90b7b916e4f297cbb99d607ec88c78c9cf8c7aaa
Gary Russell <grussell@pivotal.io>
2019-02-15 12:32:17 -0500

    Add updateCopyrights Gradle Task

--- spring-amqp/src/test/java/org/springframework/amqp/remoting/RemotingTest.java
+++ spring-amqp/src/test/java/org/springframework/amqp/remoting/RemotingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,7 +63,7 @@ public class RemotingTest {
 	 * a running rabbit.
 	 */
 	@Before
-	public void initializeTestRig() throws Exception {
+	public void initializeTestRig() {
 		// Set up the service
 		TestServiceInterface testService = new TestServiceImpl();
 		this.serviceExporter = new AmqpInvokerServiceExporter();
