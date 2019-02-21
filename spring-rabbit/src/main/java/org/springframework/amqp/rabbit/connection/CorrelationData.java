be088c6dac579269379da1c014f51493b7c7a765
Gary Russell <grussell@pivotal.io>
2019-02-21 15:49:32 -0500

    Make CorrelationData.setReturnedMessage public
    See https://github.com/spring-projects/spring-integration/issues/2759
    
    __cherry-pick to 2.1.x, 2.0.x, 1.7.x__

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/CorrelationData.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/CorrelationData.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,7 +97,12 @@ public class CorrelationData implements Correlation {
 		return this.returnedMessage;
 	}
 
-	void setReturnedMessage(Message returnedMessage) {
+	/**
+	 * Set a returned message for this correlation data.
+	 * @param returnedMessage the returned message.
+	 * @since 1.7.13
+	 */
+	public void setReturnedMessage(Message returnedMessage) {
 		this.returnedMessage = returnedMessage;
 	}
 
