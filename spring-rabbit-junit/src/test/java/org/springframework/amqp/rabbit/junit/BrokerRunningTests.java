04d014aa9beb7428b08f4dbf11a36014a516a05f
Artem Bilan <abilan@pivotal.io>
2019-03-20 21:24:08 -0400

    Fix BrokerRunningTest back to http for management
    https://build.spring.io/browse/AMQP-MASTER-1059/

--- spring-rabbit-junit/src/test/java/org/springframework/amqp/rabbit/junit/BrokerRunningTests.java
+++ spring-rabbit-junit/src/test/java/org/springframework/amqp/rabbit/junit/BrokerRunningTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2017-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ public class BrokerRunningTests {
 		brokerRunning.setPort(1234);
 		brokerRunning.setUser("fiz");
 
-		assertEquals("https://baz:15672/api/", brokerRunning.getAdminUri());
+		assertEquals("http://baz:15672/api/", brokerRunning.getAdminUri());
 		ConnectionFactory connectionFactory = brokerRunning.getConnectionFactory();
 		assertEquals("baz", connectionFactory.getHost());
 		assertEquals(1234, connectionFactory.getPort());
