525eb0ce53a8d529a87ea3a6920be417894aba8c
kc910521 <kc910521@qq.com>
2019-04-19 14:44:25 +0800

    FIX ISSUE#972
    you can get channel by extends protected method repackArgument

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/adapter/MessageListenerAdapter.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/adapter/MessageListenerAdapter.java
@@ -288,7 +288,7 @@ public class MessageListenerAdapter extends AbstractAdaptableMessageListener {
 		}
 
 		// Invoke the handler method with appropriate arguments.
-		Object[] listenerArguments = buildListenerArguments(convertedMessage);
+		Object[] listenerArguments = repackArgument(buildListenerArguments(convertedMessage), channel, message);
 		Object result = invokeListenerMethod(methodName, listenerArguments, message);
 		if (result != null) {
 			handleResult(new InvocationResult(result, null, null), message, channel);
@@ -343,6 +343,24 @@ public class MessageListenerAdapter extends AbstractAdaptableMessageListener {
 	protected Object[] buildListenerArguments(Object extractedMessage) {
 		return new Object[] {extractedMessage};
 	}
+	
+	 /**
+     * Give a chance to repack listenerArguments, with channel/message
+     * <p>
+     * ListenerArguments will always be passed into a <i>single</i> method argument, so when acknowledge type be assigned
+     * to AcknowledgeMode.MANUAL, you have to override this method for receiving Channel and Message to ack or reject.
+     * <p>
+     * This can be overridden to add argument for receiving such as Channel or Message.
+     *
+     * @param listenerArguments the content of the message
+     * @param channel           the Rabbit channel to operate on
+     * @param message           the incoming Rabbit message
+     * @return the array of arguments to be passed into the listener method (each element of the array corresponding to
+     * a distinct method argument)
+     */
+    protected Object[] repackArgument(Object[] listenerArguments, Channel channel, Message message) {
+        return listenerArguments;
+    }
 
 	/**
 	 * Invoke the specified listener method.
