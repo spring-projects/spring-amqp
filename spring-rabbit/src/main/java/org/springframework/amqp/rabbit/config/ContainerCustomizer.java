20943cca0d266a72a0d634d5c1b6bf64cb0b32dd
Gary Russell <grussell@pivotal.io>
2019-11-12 17:17:56 -0500

    Add ContainerCustomizer
    - deprecate containerConfigurer `Consumer<C>` to more easily facilitate
      Boot auto configuration.
    
    * Add docs.

---
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/ContainerCustomizer.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.amqp.rabbit.config;
+
+import org.springframework.amqp.rabbit.listener.AbstractMessageListenerContainer;
+
+/**
+ * Called by the container factory after the container is created and configured.
+ *
+ * @param <C> the container type.
+ *
+ * @author Gary Russell
+ * @since 2.2.2
+ *
+ */
+@FunctionalInterface
+public interface ContainerCustomizer<C extends AbstractMessageListenerContainer> {
+
+	/**
+	 * Configure the container.
+	 * @param container the container.
+	 */
+	void configure(C container);
+
+}
