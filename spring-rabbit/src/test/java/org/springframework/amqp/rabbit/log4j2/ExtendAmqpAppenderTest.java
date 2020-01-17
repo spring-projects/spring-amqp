da37c0ccbe68cd2e1140bb126b83620cb213243f
Francesco Scipioni <scipioni88@gmail.com>
2020-01-17 18:24:49 +0100

    fix LICENSE and logging.adoc

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/log4j2/ExtendAmqpAppenderTest.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/log4j2/ExtendAmqpAppenderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 the original author or authors.
+ * Copyright 2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ import org.springframework.test.util.ReflectionTestUtils;
  * @since 2.2.4
  */
 @RabbitAvailable
-public class ExtendAmqpAppenderTest {
+public class ExtendAmqpAppenderTests {
 
 	private static final LoggerContext LOGGER_CONTEXT = (LoggerContext) LogManager.getContext(false);
 
