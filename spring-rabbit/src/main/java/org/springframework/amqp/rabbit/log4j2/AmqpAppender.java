8622db9ee67904f596a95fb116a2f6c2fc7e9830
Gary Russell <grussell@pivotal.io>
2020-02-25 16:46:49 -0500

    GH-1162: Reinstate GH-1148 Log4j Appender Change
    Resolves https://github.com/spring-projects/spring-amqp/issues/1162
    
    Revert "Revert "GH-1148: Revert Log4j Appender Change""
    
    This reverts commit 7af0f90f0a44316f211d8f8b36d7699210b90e61.
    
    Upgrade SF to 5.2.4.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/log4j2/AmqpAppender.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/log4j2/AmqpAppender.java
@@ -126,7 +126,7 @@ public class AmqpAppender extends AbstractAppender {
 	/**
 	 * The template.
 	 */
-	private RabbitTemplate rabbitTemplate;
+	private final RabbitTemplate rabbitTemplate = new RabbitTemplate();
 
 	/**
 	 * Where LoggingEvents are queued to send.
@@ -250,11 +250,17 @@ public class AmqpAppender extends AbstractAppender {
 	 * Submit the required number of senders into the pool.
 	 */
 	private void startSenders() {
+		this.rabbitTemplate.setConnectionFactory(this.manager.connectionFactory);
 		if (this.manager.async) {
-			this.manager.senderPool = Executors.newCachedThreadPool();
 			for (int i = 0; i < this.manager.senderPoolSize; i++) {
 				this.manager.senderPool.submit(new EventSender());
 			}
+		}
+		else if (this.manager.maxSenderRetries > 0) {
+			RetryTemplate retryTemplate = new RetryTemplate();
+			RetryPolicy retryPolicy = new SimpleRetryPolicy(this.manager.maxSenderRetries);
+			retryTemplate.setRetryPolicy(retryPolicy);
+			this.rabbitTemplate.setRetryTemplate(retryTemplate);
 		}
 	}
 
@@ -281,22 +287,6 @@ public class AmqpAppender extends AbstractAppender {
 	}
 
 	protected void sendEvent(Event event, Map<?, ?> properties) {
-		synchronized (this) {
-			if (this.rabbitTemplate == null) {
-				if (this.manager.activateOptions()) {
-					this.rabbitTemplate = new RabbitTemplate(this.manager.connectionFactory);
-					if (!this.manager.async && this.manager.maxSenderRetries > 0) {
-						RetryTemplate retryTemplate = new RetryTemplate();
-						RetryPolicy retryPolicy = new SimpleRetryPolicy(this.manager.maxSenderRetries);
-						retryTemplate.setRetryPolicy(retryPolicy);
-						this.rabbitTemplate.setRetryTemplate(retryTemplate);
-					}
-				}
-				else {
-					throw new AmqpException("Cannot create template");
-				}
-			}
-		}
 		LogEvent logEvent = event.getEvent();
 		String name = logEvent.getLoggerName();
 		Level level = logEvent.getLevel();
@@ -362,7 +352,7 @@ public class AmqpAppender extends AbstractAppender {
 				message = new Message(msgBody.toString().getBytes(), amqpProps); //NOSONAR (default charset)
 			}
 			message = postProcessMessageBeforeSend(message, event);
-			this.rabbitTemplate.send(this.manager.exchangeName, routingKey, message); // NOSONAR (sync)
+			this.rabbitTemplate.send(this.manager.exchangeName, routingKey, message);
 		}
 		catch (AmqpException e) {
 			int retries = event.incrementRetries();
@@ -655,7 +645,7 @@ public class AmqpAppender extends AbstractAppender {
 			super(loggerContext, name);
 		}
 
-		boolean activateOptions() {
+		private boolean activateOptions() {
 			ConnectionFactory rabbitConnectionFactory = createRabbitConnectionFactory();
 			if (rabbitConnectionFactory != null) {
 				Assert.state(this.applicationId != null, "applicationId is required");
@@ -677,6 +667,7 @@ public class AmqpAppender extends AbstractAppender {
 							this.clientConnectionProperties);
 				}
 				setUpExchangeDeclaration();
+				this.senderPool = Executors.newCachedThreadPool();
 				return true;
 			}
 			return false;
@@ -1191,8 +1182,11 @@ public class AmqpAppender extends AbstractAppender {
 			}
 
 			AmqpAppender appender = buildInstance(this.name, this.filter, theLayout, this.ignoreExceptions, manager, eventQueue);
-			appender.startSenders();
-			return appender;
+			if (manager.activateOptions()) {
+				appender.startSenders();
+				return appender;
+			}
+			return null;
 		}
 
 		/**
