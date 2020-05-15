8e615d0f270c423a34008694528e4fa6b7d4cc1f
Gary Russell <grussell@pivotal.io>
2020-05-15 11:04:27 -0400

    GH-1198: Support AddressResolver
    Resolves https://github.com/spring-projects/spring-amqp/issues/1198
    
    **cherry-pick to 2.2.x, 2.1.x**
    
    # Conflicts:
    #	spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/ConnectionFactoryParser.java
    #	spring-rabbit/src/test/java/org/springframework/amqp/rabbit/connection/CachingConnectionFactoryTests.java
    
    # Conflicts:
    #	spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/AbstractConnectionFactory.java
    #	spring-rabbit/src/test/java/org/springframework/amqp/rabbit/config/ConnectionFactoryParserTests.java
    #	spring-rabbit/src/test/java/org/springframework/amqp/rabbit/connection/CachingConnectionFactoryTests.java

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/config/ConnectionFactoryParserTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/config/ConnectionFactoryParserTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -129,4 +129,10 @@ public final class ConnectionFactoryParserTests {
 				"rabbitConnectionFactory.threadFactory"));
 	}
 
+	@Test
+	void testResolver() {
+		CachingConnectionFactory connectionFactory = beanFactory.getBean("resolved", CachingConnectionFactory.class);
+		assertThat(TestUtils.getPropertyValue(connectionFactory, "addressResolver"))
+				.isSameAs(this.beanFactory.getBean("resolver"));
+	}
 }
