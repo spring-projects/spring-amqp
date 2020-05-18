751326ee6305687477c791e9b2f4d48bd4fa27f2
Gary Russell <grussell@pivotal.io>
2020-05-18 14:20:18 -0400

    GH-1201: Native BatchMessageListener Support
    Resolves https://github.com/spring-projects/spring-amqp/issues/1201
    
    Support de-batching producer batches to a `List<Meessage>` with native
    `BatchMessageListener`s; previously this was only possible with
    `@RabbitListener` which does the debatching itself.
    
    * Fix race in test.
    
    https://github.com/spring-projects/spring-amqp/pull/1202#issuecomment-629436933
    
    The test has a very short timeout; there is a race such that the channel won't be
    physically closed if we don't get the `ConsumeOK` in time.
    
    This won't really cause a problem in a real application, but this change will
    prevent the test from sporadically failing.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitTemplate.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/core/RabbitTemplate.java
@@ -2631,6 +2631,7 @@ public class RabbitTemplate extends RabbitAccessor // NOSONAR type line count
 			future.completeExceptionally(
 					new ConsumeOkNotReceivedException("Blocking receive, consumer failed to consume within "
 							+ timeoutMillis + " ms: " + consumer));
+			RabbitUtils.setPhysicalCloseRequired(channel, true);
 		}
 		return consumer;
 	}
