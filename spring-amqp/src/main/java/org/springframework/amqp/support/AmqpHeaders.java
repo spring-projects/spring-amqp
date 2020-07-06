144bf812bdd090a1c9893dca7bd39a39fa31a55c
Gary Russell <grussell@vmware.com>
2020-07-06 16:17:38 -0400

    GH-1219: Fix header mapping for replies (@SendTo)
    Resolves https://github.com/spring-projects/spring-amqp/issues/1219
    
    The headers were mapped after message conversion.
    This prevented using a `ContentTypeDelegatingMessageConverter` because
    the content type was not set.
    
    Add a header to control whether the user or converter gets to set the
    content type property in the final message.
    
    **cherry-pick to 2.2.x, 2.1.x, 1.7.x**
    
    # Conflicts:
    #	spring-amqp/src/main/java/org/springframework/amqp/support/AmqpHeaders.java
    #	spring-amqp/src/main/java/org/springframework/amqp/support/converter/MessagingMessageConverter.java
    #	src/reference/asciidoc/amqp.adoc

--- spring-amqp/src/main/java/org/springframework/amqp/support/AmqpHeaders.java
+++ spring-amqp/src/main/java/org/springframework/amqp/support/AmqpHeaders.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,6 +48,8 @@ public abstract class AmqpHeaders {
 	public static final String CONTENT_LENGTH = PREFIX + "contentLength";
 
 	public static final String CONTENT_TYPE = MessageHeaders.CONTENT_TYPE;
+
+	public static final String CONTENT_TYPE_CONVERTER_WINS = PREFIX + "contentTypeConverterWins";
 
 	public static final String CORRELATION_ID = PREFIX + "correlationId";
 
