bdef0fd1bc774f51f2e36b7a41df8c6f4864e1f9
Gary Russell <grussell@vmware.com>
2021-03-30 11:41:57 -0400

    Fix tests; remove null test in `Message.toString()`.

--- spring-amqp/src/main/java/org/springframework/amqp/core/Message.java
+++ spring-amqp/src/main/java/org/springframework/amqp/core/Message.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -119,9 +119,7 @@ public class Message implements Serializable {
 		StringBuilder buffer = new StringBuilder();
 		buffer.append("(");
 		buffer.append("Body:'").append(this.getBodyContentAsString()).append("'");
-		if (this.messageProperties != null) {
-			buffer.append(" ").append(this.messageProperties.toString());
-		}
+		buffer.append(" ").append(this.messageProperties.toString());
 		buffer.append(")");
 		return buffer.toString();
 	}
--- spring-amqp/src/test/java/org/springframework/amqp/core/MessageTests.java
+++ spring-amqp/src/test/java/org/springframework/amqp/core/MessageTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,13 +56,13 @@ public class MessageTests {
 
 	@Test
 	public void toStringForNullMessageProperties() {
-		Message message = new Message(new byte[0], null);
+		Message message = new Message(new byte[0]);
 		assertThat(message.toString()).isNotNull();
 	}
 
 	@Test
 	public void toStringForNonStringMessageBody() {
-		Message message = new Message(SerializationUtils.serialize(new Date()), null);
+		Message message = new Message(SerializationUtils.serialize(new Date()));
 		assertThat(message.toString()).isNotNull();
 	}
 
