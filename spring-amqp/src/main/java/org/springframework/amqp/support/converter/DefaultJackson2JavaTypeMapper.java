bb06e92a81248ba03ab96b7b45dfa29a911c0b03
Gary Russell <grussell@vmware.com>
2021-09-27 15:46:52 -0400

    Upgrade checkstyle; fix violations
    - a checkstyle bug prevented detection of javadoc tag ordering
    - `allowMissingJavadoc` is no longer available on `JavadocMethod`
      (replaced by `MissingJavadocMethod`)

--- spring-amqp/src/main/java/org/springframework/amqp/support/converter/DefaultJackson2JavaTypeMapper.java
+++ spring-amqp/src/main/java/org/springframework/amqp/support/converter/DefaultJackson2JavaTypeMapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,8 +52,8 @@ public class DefaultJackson2JavaTypeMapper extends AbstractJavaTypeMapper implem
 	/**
 	 * Return the precedence.
 	 * @return the precedence.
-	 * @see #setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence)
 	 * @since 1.6.
+	 * @see #setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence)
 	 */
 	@Override
 	public TypePrecedence getTypePrecedence() {
