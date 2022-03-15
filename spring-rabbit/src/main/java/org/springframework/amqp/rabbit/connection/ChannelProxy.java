8eeb931064c0d809b993e012be1641e39995d77a
Leonardo Ferreira <mail@leonardoferreira.com.br>
2022-03-15 12:45:54 -0300

    Fix eviction logic in the PooledChannelCF
    The method `destroyObject()` is called by the eviction process of Apache Pool2, 
    but it tries to do a logical close that puts the channel back on the pool causing a 
    `java.lang.IllegalStateException: Returned object not currently part of this pool and object lost (abandoned).`
    
    * Extract the `targetChannel` from the proxy and call a `physicalClose()` in the `destroyObject()` for a proper pool eviction
    
    **Cherry-pick to `2.4.x` & `2.3.x`**

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/ChannelProxy.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/ChannelProxy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 
 package org.springframework.amqp.rabbit.connection;
 
+import org.springframework.aop.RawTargetAccess;
+
 import com.rabbitmq.client.Channel;
 
 /**
@@ -24,9 +26,10 @@ import com.rabbitmq.client.Channel;
  *
  * @author Mark Pollack
  * @author Gary Russell
+ * @author Leonardo Ferreira
  * @see CachingConnectionFactory
  */
-public interface ChannelProxy extends Channel {
+public interface ChannelProxy extends Channel, RawTargetAccess {
 
 	/**
 	 * Return the target Channel of this proxy.
