f63c1ed1228028ec46694c1f92629d19e6216b2e
Gary Russell <grussell@vmware.com>
2022-05-18 16:05:14 -0400

    GH-1459: Fix Javadocs

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/ListenerContainerFactoryBean.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/ListenerContainerFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2021 the original author or authors.
+ * Copyright 2016-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -438,7 +438,7 @@ public class ListenerContainerFactoryBean extends AbstractFactoryBean<AbstractMe
 
 	/**
 	 * Set to false to disable micrometer listener timers.
-	 * @param micrometerEnabled false to disable.
+	 * @param enabled false to disable.
 	 * @since 2.4.6
 	 */
 	public void setMicrometerEnabled(boolean enabled) {
@@ -457,7 +457,7 @@ public class ListenerContainerFactoryBean extends AbstractFactoryBean<AbstractMe
 	/**
 	 * Set a {@link ContainerCustomizer} that is invoked after a container is created and
 	 * configured to enable further customization of the container.
-	 * @param containerCustomizer the customizer.
+	 * @param customizer the customizer.
 	 * @since 2.4.6
 	 */
 	public void setSMLCCustomizer(ContainerCustomizer<SimpleMessageListenerContainer> customizer) {
@@ -467,7 +467,7 @@ public class ListenerContainerFactoryBean extends AbstractFactoryBean<AbstractMe
 	/**
 	 * Set a {@link ContainerCustomizer} that is invoked after a container is created and
 	 * configured to enable further customization of the container.
-	 * @param containerCustomizer the customizer.
+	 * @param customizer the customizer.
 	 * @since 2.4.6
 	 */
 	public void setDMLCCustomizer(ContainerCustomizer<DirectMessageListenerContainer> customizer) {
