0a2b8c90a8d09f9affdf825d68184002fb1246b8
Alexey Anufriev <ice.sumy@gmail.com>
2022-05-23 00:49:38 +0200

    chore: remove redundant space is string representation

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/SimpleConnection.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/connection/SimpleConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -152,7 +152,7 @@ public class SimpleConnection implements Connection, NetworkConnection {
 	public String toString() {
 		return "SimpleConnection@"
 				+ ObjectUtils.getIdentityHexString(this)
-				+ " [delegate=" + this.delegate + ", localPort= " + getLocalPort() + "]";
+				+ " [delegate=" + this.delegate + ", localPort=" + getLocalPort() + "]";
 	}
 
 }
