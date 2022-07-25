016980ac47b2105f95d7c2605f31f99c8629d70d
Gary Russell <grussell@vmware.com>
2022-07-25 10:46:32 -0400

    GH-1474: Fix BatchingStrategy Propagation
    See https://github.com/spring-projects/spring-amqp/issues/1474
    (Does not resolve because 2 issues are reported there).
    
    `BatchingStrategy` was not set by container factory.
    
    **cherry-pick to 2.4.x**

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/AbstractRabbitListenerEndpoint.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/AbstractRabbitListenerEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2021 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -308,6 +308,7 @@ public abstract class AbstractRabbitListenerEndpoint implements RabbitListenerEn
 		this.batchListener = batchListener;
 	}
 
+	@Override
 	@Nullable
 	public BatchingStrategy getBatchingStrategy() {
 		return this.batchingStrategy;
