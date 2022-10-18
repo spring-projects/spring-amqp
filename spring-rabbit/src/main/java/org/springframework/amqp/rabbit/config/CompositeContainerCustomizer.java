b63cd5bd35fc971b7207aeb0bc91c482bf2c66d8
Gary Russell <grussell@vmware.com>
2022-10-18 09:33:43 -0400

    GH-1517: Docs and Polishing for Composite Cust.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/CompositeContainerCustomizer.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/CompositeContainerCustomizer.java
@@ -1,23 +1,52 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.amqp.rabbit.config;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.util.Assert;
 
 /**
- * Implementation of {@link ContainerCustomizer<C>} providing the configuration of multiple customizers at the same time.
+ * Implementation of {@link ContainerCustomizer<C>} providing the configuration of
+ * multiple customizers at the same time.
+ *
+ * @param <C> the container type.
+ *
+ * @author Rene Felgentraeger
+ * @author Gary Russell
  */
 public class CompositeContainerCustomizer<C extends MessageListenerContainer> implements ContainerCustomizer<C> {
 
-	private final List<ContainerCustomizer<C>> customizers;
+	private final List<ContainerCustomizer<C>> customizers = new ArrayList<>();
 
+	/**
+	 * Create an instance with the provided delegate customizers.
+	 * @param customizers the customizers.
+	 */
 	public CompositeContainerCustomizer(List<ContainerCustomizer<C>> customizers) {
 		Assert.notNull(customizers, "At least one customizer must be present");
-		this.customizers = customizers;
+		this.customizers.addAll(customizers);
 	}
 
 	@Override
 	public void configure(C container) {
 		customizers.forEach(c -> c.configure(container));
 	}
+
 }
