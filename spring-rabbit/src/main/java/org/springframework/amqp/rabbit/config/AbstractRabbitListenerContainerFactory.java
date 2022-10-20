670e842d83e5206f24bfb9a9807999b5a733959a
Gary Russell <grussell@vmware.com>
2022-10-20 16:19:02 -0400

    Fix link in What's New; add observation properties to container factory.

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/AbstractRabbitListenerContainerFactory.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/config/AbstractRabbitListenerContainerFactory.java
@@ -32,6 +32,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.AbstractMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.MessageAckListener;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpoint;
+import org.springframework.amqp.rabbit.support.micrometer.RabbitListenerObservationConvention;
 import org.springframework.amqp.support.ConsumerTagStrategy;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.amqp.utils.JavaUtils;
@@ -117,6 +118,12 @@ public abstract class AbstractRabbitListenerContainerFactory<C extends AbstractM
 	private Boolean deBatchingEnabled;
 
 	private MessageAckListener messageAckListener;
+
+	private Boolean micrometerEnabled;
+
+	private Boolean observationEnabled;
+
+	private RabbitListenerObservationConvention observationConvention;
 
 	/**
 	 * @param connectionFactory The connection factory.
@@ -336,6 +343,37 @@ public abstract class AbstractRabbitListenerContainerFactory<C extends AbstractM
 		this.messageAckListener = messageAckListener;
 	}
 
+	/**
+	 * Set to false to disable micrometer listener timers. When true, ignored
+	 * if {@link #setObservationEnabled(boolean)} is set to true.
+	 * @param micrometerEnabled false to disable.
+	 * @since 3.0
+	 * @see #setObservationEnabled(boolean)
+	 */
+	public void setMicrometerEnabled(boolean micrometerEnabled) {
+		this.micrometerEnabled = micrometerEnabled;
+	}
+
+	/**
+	 * Enable observation via micrometer; disables basic Micrometer timers enabled
+	 * by {@link #setMicrometerEnabled(boolean)}.
+	 * @param observationEnabled true to enable.
+	 * @since 3.0
+	 * @see #setMicrometerEnabled(boolean)
+	 */
+	public void setObservationEnabled(boolean observationEnabled) {
+		this.observationEnabled = observationEnabled;
+	}
+
+	/**
+	 * Set an observation convention; used to add additional key/values to observations.
+	 * @param observationConvention the convention.
+	 * @since 3.0
+	 */
+	public void setObservationConvention(RabbitListenerObservationConvention observationConvention) {
+		this.observationConvention = observationConvention;
+	}
+
 	@Override
 	public C createListenerContainer(RabbitListenerEndpoint endpoint) {
 		C instance = createContainerInstance();
@@ -370,7 +408,10 @@ public abstract class AbstractRabbitListenerContainerFactory<C extends AbstractM
 			.acceptIfNotNull(this.afterReceivePostProcessors, instance::setAfterReceivePostProcessors)
 			.acceptIfNotNull(this.deBatchingEnabled, instance::setDeBatchingEnabled)
 			.acceptIfNotNull(this.messageAckListener, instance::setMessageAckListener)
-			.acceptIfNotNull(this.batchingStrategy, instance::setBatchingStrategy);
+			.acceptIfNotNull(this.batchingStrategy, instance::setBatchingStrategy)
+			.acceptIfNotNull(this.micrometerEnabled, instance::setMicrometerEnabled)
+			.acceptIfNotNull(this.observationEnabled, instance::setObservationEnabled)
+			.acceptIfNotNull(this.observationConvention, instance::setObservationConvention);
 		if (this.batchListener && this.deBatchingEnabled == null) {
 			// turn off container debatching by default for batch listeners
 			instance.setDeBatchingEnabled(false);
