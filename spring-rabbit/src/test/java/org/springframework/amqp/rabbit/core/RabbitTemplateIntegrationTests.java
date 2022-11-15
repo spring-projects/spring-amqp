6975341ed72463eee8b52596f644eac63857f1e2
Gary Russell <grussell@vmware.com>
2022-11-15 12:57:09 -0500

    GH-1533: Template Receive with Consumer Args
    Resolves https://github.com/spring-projects/spring-amqp/issues/1533
    
    Allow setting consumer arguments when using non-zero receive
    timeouts using the `RabbitTemplate`.
    
    **cherry-pick to 2.4.x**

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/core/RabbitTemplateIntegrationTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/core/RabbitTemplateIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -335,8 +335,10 @@ public class RabbitTemplateIntegrationTests {
 			}
 
 			@Override
-			public String basicConsume(String queue, Consumer callback) throws IOException {
-				return super.basicConsume(queue, new MockConsumer(callback));
+			public String basicConsume(String queue, boolean autoAck, Map<String, Object> args, Consumer callback)
+					throws IOException {
+
+				return super.basicConsume(queue, autoAck, args, new MockConsumer(callback));
 			}
 
 		}
