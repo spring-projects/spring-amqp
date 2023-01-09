324df8560267325e8a28c88b0b2c1d6abd7c5332
Gary Russell <grussell@vmware.com>
2023-01-09 15:23:27 -0500

    GH-1550: Fix Mono Return Type Detection
    Resolves https://github.com/spring-projects/spring-amqp/issues/1550
    
    **cherry-pick to 2.4.x**

--- spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/adapter/MonoHandler.java
+++ spring-rabbit/src/main/java/org/springframework/amqp/rabbit/listener/adapter/MonoHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2021-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,10 @@ final class MonoHandler { // NOSONAR - pointless to name it ..Utils|Helper
 
 	static boolean isMono(Object result) {
 		return result instanceof Mono;
+	}
+
+	static boolean isMono(Class<?> resultType) {
+		return Mono.class.isAssignableFrom(resultType);
 	}
 
 	@SuppressWarnings("unchecked")
