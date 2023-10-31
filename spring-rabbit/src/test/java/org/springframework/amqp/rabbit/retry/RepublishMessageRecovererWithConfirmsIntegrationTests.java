e7d2ca92d18838c4e93af117e77d7cbab619de6f
Gary Russell <grussell@vmware.com>
2023-10-31 09:38:30 -0400

    GH-2552: Fix Builder Argument Types maxLength etc
    Resolves https://github.com/spring-projects/spring-amqp/issues/2552
    
    Variables map to Erlang ints which are 8 bytes, so need to be long in Java.

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/retry/RepublishMessageRecovererWithConfirmsIntegrationTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/retry/RepublishMessageRecovererWithConfirmsIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 the original author or authors.
+ * Copyright 2018-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,7 +103,7 @@ class RepublishMessageRecovererWithConfirmsIntegrationTests {
 		RabbitTemplate template = new RabbitTemplate(ccf);
 		RabbitAdmin admin = new RabbitAdmin(ccf);
 		Queue queue = QueueBuilder.durable(QUEUE + ".nack")
-				.maxLength(1)
+				.maxLength(1L)
 				.overflow(Overflow.rejectPublish)
 				.build();
 		admin.declareQueue(queue);
