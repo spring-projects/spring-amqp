54a429b16bf3b9c7b09e2128be32accfca4851ce
Artem Bilan <artem.bilan@broadcom.com>
2026-01-02 15:23:24 -0500

    Add exception conversion utils into `ProtonUtils`

--- spring-amqp-client/src/main/java/org/springframework/amqp/client/ProtonUtils.java
+++ spring-amqp-client/src/main/java/org/springframework/amqp/client/ProtonUtils.java
@@ -18,11 +18,30 @@ package org.springframework.amqp.client;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
 
+import javax.net.ssl.SSLException;
+
+import org.apache.qpid.protonj2.client.ErrorCondition;
 import org.apache.qpid.protonj2.client.Message;
+import org.apache.qpid.protonj2.client.exceptions.ClientConnectionRemotelyClosedException;
+import org.apache.qpid.protonj2.client.exceptions.ClientConnectionSecurityException;
 import org.apache.qpid.protonj2.client.exceptions.ClientException;
+import org.apache.qpid.protonj2.client.exceptions.ClientLinkRemotelyClosedException;
+import org.apache.qpid.protonj2.client.exceptions.ClientResourceRemotelyClosedException;
+import org.apache.qpid.protonj2.client.exceptions.ClientSessionRemotelyClosedException;
 import org.jspecify.annotations.Nullable;
 
+import org.springframework.amqp.AmqpAuthenticationException;
+import org.springframework.amqp.AmqpConnectException;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.AmqpResourceNotAvailableException;
+import org.springframework.amqp.AmqpTimeoutException;
+import org.springframework.amqp.UncategorizedAmqpException;
 import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.util.StringUtils;
@@ -35,6 +54,12 @@ import org.springframework.util.StringUtils;
  * @since 4.1
  */
 public final class ProtonUtils {
+
+	private static final String ERROR_UNAUTHORIZED_ACCESS = "amqp:unauthorized-access";
+
+	private static final String ERROR_NOT_FOUND = "amqp:not-found";
+
+	private static final String ERROR_RESOURCE_DELETED = "amqp:resource-deleted";
 
 	/**
 	 * Convert a Spring AMQP message to a ProtonJ message.
@@ -83,8 +108,155 @@ public final class ProtonUtils {
 			return protonMessage;
 		}
 		catch (ClientException ex) {
-			throw new AmqpClientException(ex);
+			throw convert(ex);
 		}
+	}
+
+	static <T> Supplier<T> toUncheckedSupplier(Future<T> future, long timeout) {
+		return () -> {
+			try {
+				if (timeout > 0) {
+					return future.get(timeout, TimeUnit.MILLISECONDS);
+				}
+				else {
+					return future.get();
+				}
+			}
+			catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+				throw new UncategorizedAmqpException(ex);
+			}
+			catch (TimeoutException ex) {
+				throw convert(ex);
+			}
+			catch (ExecutionException ex) {
+				throw convert(ex);
+			}
+		};
+	}
+
+	static AmqpException convert(Exception ex) {
+		if (ex instanceof AmqpException amqpException) {
+			return amqpException;
+		}
+		else if (ex instanceof ClientException clientException) {
+			return convert(clientException);
+		}
+		else if (ex instanceof TimeoutException) {
+			return new AmqpTimeoutException(ex);
+		}
+		else {
+			return new AmqpException(ex);
+		}
+	}
+
+	static AmqpException convert(ClientException ex) {
+		return convert(ex, null);
+	}
+
+	static AmqpException convert(ExecutionException ex) {
+		Throwable cause = ex.getCause();
+		if (cause instanceof ClientException clientException) {
+			return convert(clientException);
+		}
+		else {
+			return new AmqpException(cause == null ? ex : cause);
+		}
+	}
+
+	static AmqpException convert(ClientException ex, @Nullable String format, Object... args) {
+		return convert(ex, true, format, args);
+	}
+
+	private static AmqpException convert(ClientException ex, boolean checkCause, @Nullable String format,
+			Object... args) {
+
+		String message = format != null ? String.format(format, args) : null;
+		AmqpException result;
+		if (ex.getCause() instanceof SSLException sslException) {
+			result = new AmqpAuthenticationException(sslException);
+		}
+		else if (ex instanceof ClientConnectionSecurityException) {
+			result = new AmqpAuthenticationException(ex);
+		}
+		else {
+			String exMessage = ex.getMessage();
+			if (isNetworkError(ex)) {
+				result = new AmqpConnectException(exMessage != null ? exMessage : "Connection error", ex);
+			}
+			else if (ex instanceof ClientSessionRemotelyClosedException
+					|| ex instanceof ClientLinkRemotelyClosedException) {
+
+				ErrorCondition errorCondition =
+						((ClientResourceRemotelyClosedException) ex).getErrorCondition();
+
+				if (isUnauthorizedAccess(errorCondition)) {
+					result = new AmqpAuthenticationException(ex);
+				}
+				else {
+					result = new AmqpResourceNotAvailableException(
+							exMessage != null ? exMessage : "Resource is not available", ex);
+				}
+			}
+			else if (ex instanceof ClientConnectionRemotelyClosedException clientConnectionRemotelyClosedException) {
+				ErrorCondition errorCondition = clientConnectionRemotelyClosedException.getErrorCondition();
+				if (isNetworkError(ex) || !isUnauthorizedAccess(errorCondition)) {
+					result = new AmqpConnectException(exMessage != null ? exMessage : "Connection error", ex);
+				}
+				else {
+					result = new AmqpException(exMessage != null ? exMessage : "Connection error", ex);
+				}
+			}
+			else {
+				result = new AmqpException(message, ex);
+			}
+		}
+		if (checkCause
+				&& AmqpException.class.getName().equals(result.getClass().getName())
+				&& ex.getCause() instanceof ClientException clientException) {
+			// we end up with a generic exception, we try to narrow down with the cause
+			result = convert(clientException, false, format, args);
+		}
+		return result;
+	}
+
+	static boolean resourceDeleted(ClientResourceRemotelyClosedException ex) {
+		return isResourceDeleted(ex.getErrorCondition());
+	}
+
+	static boolean notFound(ClientResourceRemotelyClosedException ex) {
+		return isNotFound(ex.getErrorCondition());
+	}
+
+	static boolean unauthorizedAccess(ClientResourceRemotelyClosedException ex) {
+		return isUnauthorizedAccess(ex.getErrorCondition());
+	}
+
+	private static boolean isUnauthorizedAccess(ErrorCondition errorCondition) {
+		return errorConditionEquals(errorCondition, ERROR_UNAUTHORIZED_ACCESS);
+	}
+
+	private static boolean isNotFound(ErrorCondition errorCondition) {
+		return errorConditionEquals(errorCondition, ERROR_NOT_FOUND);
+	}
+
+	private static boolean isResourceDeleted(ErrorCondition errorCondition) {
+		return errorConditionEquals(errorCondition, ERROR_RESOURCE_DELETED);
+	}
+
+	private static boolean errorConditionEquals(ErrorCondition errorCondition, String expected) {
+		return expected.equals(errorCondition.condition());
+	}
+
+	private static boolean isNetworkError(ClientException e) {
+		if (e instanceof ClientConnectionRemotelyClosedException) {
+			String message = e.getMessage();
+			if (message != null) {
+				message = message.toLowerCase();
+				return message.contains("connection reset") || message.contains("connection refused");
+			}
+		}
+		return false;
 	}
 
 	private ProtonUtils() {
