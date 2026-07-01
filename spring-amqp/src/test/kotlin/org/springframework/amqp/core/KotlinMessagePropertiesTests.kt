946e7c6a008d4311e28d7d014dacfc21d7db9eaf
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2026-07-01 21:36:10 +0700

    GH-3487: Apply Nullability to MessageProperties
    For Kotlin, nullable and non-nullable types are different types.
    Since the setter method in the Java class is not annotated with `@Nullable`, Kotlin treats the property as a val (read-only).
    
    Fixes: https://github.com/spring-projects/spring-amqp/issues/3487
    
    * Use `mock()` and add `@since` tag
    
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
    
    (cherry picked from commit ec584a3781264a549a609530e0b1aae02481fb5a)

---
+++ spring-amqp/src/test/kotlin/org/springframework/amqp/core/KotlinMessagePropertiesTests.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2026-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.amqp.core
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
+import assertk.assertions.isTrue
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import java.lang.reflect.Type
+import java.util.Date
+
+/**
+ * @author Ngoc Nhan
+ *
+ * @since 4.0.5
+ */
+class KotlinMessagePropertiesTests {
+
+	@Test
+	fun testSetterForNullabilityFields () {
+		val currentDate = Date()
+		val amqpAcknowledgment = mock(AmqpAcknowledgment::class.java)
+		val inferredArgumentType = mock(Type::class.java)
+		val messageProperties = MessageProperties()
+			messageProperties.amqpAcknowledgment = amqpAcknowledgment
+			messageProperties.appId = "appId"
+			messageProperties.clusterId = "clusterId"
+			messageProperties.consumerQueue = "consumerQueue"
+			messageProperties.consumerTag = "consumerTag"
+			messageProperties.expiration = "expiration"
+			messageProperties.inferredArgumentType = inferredArgumentType
+			messageProperties.messageCount = 1
+			messageProperties.messageId = "messageId"
+			messageProperties.receivedDelayLong = 1
+			messageProperties.receivedDeliveryMode = MessageDeliveryMode.PERSISTENT
+			messageProperties.receivedExchange = "receivedExchange"
+			messageProperties.receivedRoutingKey = "receivedRoutingKey"
+			messageProperties.receivedUserId = "receivedUserId"
+			messageProperties.redelivered = true
+			messageProperties.timestamp = currentDate
+			messageProperties.type = "type"
+			messageProperties.userId = "userId"
+
+		assertThat(messageProperties.amqpAcknowledgment).isEqualTo(amqpAcknowledgment)
+		assertThat(messageProperties.appId).isEqualTo("appId")
+		assertThat(messageProperties.clusterId).isEqualTo("clusterId")
+		assertThat(messageProperties.consumerQueue).isEqualTo("consumerQueue")
+		assertThat(messageProperties.consumerTag).isEqualTo("consumerTag")
+		assertThat(messageProperties.expiration).isEqualTo("expiration")
+		assertThat(messageProperties.inferredArgumentType).isEqualTo(inferredArgumentType)
+		assertThat(messageProperties.messageCount).isEqualTo(1)
+		assertThat(messageProperties.messageId).isEqualTo("messageId")
+		assertThat(messageProperties.receivedDelayLong).isEqualTo(1)
+		assertThat(messageProperties.receivedDeliveryMode).isEqualTo(MessageDeliveryMode.PERSISTENT)
+		assertThat(messageProperties.receivedExchange).isEqualTo("receivedExchange")
+		assertThat(messageProperties.receivedRoutingKey).isEqualTo("receivedRoutingKey")
+		assertThat(messageProperties.receivedUserId).isEqualTo("receivedUserId")
+		assertThat(messageProperties.redelivered).isNotNull().isTrue()
+		assertThat(messageProperties.timestamp).isEqualTo(currentDate)
+		assertThat(messageProperties.type).isEqualTo("type")
+		assertThat(messageProperties.userId).isEqualTo("userId")
+	}
+
+}
