f0dd10fc37f8f3802b76850ec4329add73c8378e
Artem Bilan <artem.bilan@broadcom.com>
2026-08-28 15:42:24 -0400

    GH-3601: Honor `autoStartup` after `RabbitListenerEndpointRegistry` restart
    Fixes: https://github.com/spring-projects/spring-amqp/pull/3601
    
    The `contextRefreshed` flag is set on `ContextRefreshedEvent` and never
    cleared, so any `start()` after the first refresh - including one
    triggered by `ConfigurableApplicationContext.restart()` (Spring
    Framework 7.0.3+ test context pause/resume) - unconditionally starts
    all listener containers, ignoring `autoStartup=false`.
    
    Reset `contextRefreshed` in `stop()` and `stop(Runnable)` so a
    subsequent `start()`/`restart()` re-applies each container's
    `autoStartup` setting again.
    
    Same problem and fix as Spring for Apache Kafka:
    https://github.com/spring-projects/spring-kafka/pull/4568
    
    * Fix Checkstyle violations
    
    * Convert `RabbitListenerEndpointRegistry.AggregatingCallback` to the `record`
    
    (cherry picked from commit 1f06f4a5fa753c072003798c6195760c85d7c73b)

--- spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/RabbitListenerEndpointRegistryTests.java
+++ spring-rabbit/src/test/java/org/springframework/amqp/rabbit/listener/RabbitListenerEndpointRegistryTests.java
@@ -18,16 +18,24 @@ package org.springframework.amqp.rabbit.listener;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.amqp.core.MessageListenerContainer;
 import org.springframework.amqp.rabbit.config.RabbitListenerContainerTestFactory;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerEndpoint;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.support.GenericApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 /**
  *
  * @author Stephane Nicoll
  * @author Gary Russell
+ * @author Artem Bilan
  */
 public class RabbitListenerEndpointRegistryTests {
 
@@ -38,19 +46,19 @@ public class RabbitListenerEndpointRegistryTests {
 	@Test
 	public void createWithNullEndpoint() {
 		assertThatIllegalArgumentException()
-			.isThrownBy(() -> registry.registerListenerContainer(null, containerFactory));
+				.isThrownBy(() -> registry.registerListenerContainer(null, containerFactory));
 	}
 
 	@Test
 	public void createWithNullEndpointId() {
 		assertThatIllegalArgumentException()
-			.isThrownBy(() -> registry.registerListenerContainer(new SimpleRabbitListenerEndpoint(), containerFactory));
+				.isThrownBy(() -> registry.registerListenerContainer(new SimpleRabbitListenerEndpoint(), containerFactory));
 	}
 
 	@Test
 	public void createWithNullContainerFactory() {
 		assertThatIllegalArgumentException()
-			.isThrownBy(() -> registry.registerListenerContainer(createEndpoint("foo", "myDestination"), null));
+				.isThrownBy(() -> registry.registerListenerContainer(createEndpoint("foo", "myDestination"), null));
 	}
 
 	@Test
@@ -58,7 +66,43 @@ public class RabbitListenerEndpointRegistryTests {
 		registry.registerListenerContainer(createEndpoint("test", "queue"), containerFactory);
 
 		assertThatIllegalStateException()
-			.isThrownBy(() -> registry.registerListenerContainer(createEndpoint("test", "queue"), containerFactory));
+				.isThrownBy(() -> registry.registerListenerContainer(createEndpoint("test", "queue"), containerFactory));
+	}
+
+	@Test
+	public void stopThenStartHonorsAutoStartupFalse() {
+		GenericApplicationContext context = new GenericApplicationContext();
+		context.refresh();
+		registry.setApplicationContext(context);
+		MessageListenerContainer container = registerAutoStartupFalseContainer("listener");
+		registry.start();
+		verify(container, never()).start();
+		registry.onApplicationEvent(new ContextRefreshedEvent(context));
+		registry.stop();
+		registry.start();
+		verify(container, never()).start();
+	}
+
+	@Test
+	public void startAfterRefreshWithoutStopStartsContainerRegardlessOfAutoStartup() {
+		GenericApplicationContext context = new GenericApplicationContext();
+		context.refresh();
+		registry.setApplicationContext(context);
+		MessageListenerContainer container = registerAutoStartupFalseContainer("listener");
+		registry.onApplicationEvent(new ContextRefreshedEvent(context));
+		registry.start();
+		verify(container).start();
+	}
+
+	private MessageListenerContainer registerAutoStartupFalseContainer(String id) {
+		RabbitListenerEndpoint endpoint = mock();
+		given(endpoint.getId()).willReturn(id);
+		RabbitListenerContainerFactory<MessageListenerContainer> factory = mock();
+		MessageListenerContainer container = mock();
+		given(container.isAutoStartup()).willReturn(false);
+		given(factory.createListenerContainer(endpoint)).willReturn(container);
+		registry.registerListenerContainer(endpoint, factory);
+		return container;
 	}
 
 	private SimpleRabbitListenerEndpoint createEndpoint(String id, String queueName) {
